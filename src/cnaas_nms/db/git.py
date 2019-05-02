8bad4e284607ebc7872d5319efe4c9e45b3d63eb
Johan Marcusson <johan.marcusson@sunet.se>
2019-05-02 13:50:29 +0200

    Make template repo use platform dir structure and make sync_template use real settings repo instead of template from cnaas-nms src

--- src/cnaas_nms/db/git.py
+++ src/cnaas_nms/db/git.py
@@ -1,6 +1,6 @@
 import enum
 import os
-from typing import List
+from typing import Set, Tuple
 
 from git import Repo
 from git import InvalidGitRepositoryError, NoSuchPathError
@@ -118,49 +118,70 @@ def refresh_repo(repo_type: RepoType = RepoType.TEMPLATES) -> str:
         try:
             get_settings()
         except SettingsSyntaxError as e:
+            logger.exception("Error in settings repo configuration: {}".format(str(e)))
             raise e
 
     if repo_type == RepoType.TEMPLATES:
-        print("DEBUG20: {}".format(changed_files))
+        logger.debug("Files changed in template repository: {}".format(changed_files))
         updated_devtypes = template_syncstatus(updated_templates=changed_files)
         with sqla_session() as session:
             devtype: DeviceType
-            for devtype in updated_devtypes:
-                Device.set_devtype_syncstatus(session, devtype, syncstatus=False)
+            for devtype, platform in updated_devtypes:
+                Device.set_devtype_syncstatus(session, devtype, platform, syncstatus=False)
 
     return ret
 
 
-def template_syncstatus(updated_templates: set) -> List[DeviceType]:
+def template_syncstatus(updated_templates: set) -> Set[Tuple[DeviceType, str]]:
     """Determine what device types have become unsynchronized because
     of updated template files."""
+    unsynced_devtype = set()
     with open('/etc/cnaas-nms/repository.yml', 'r') as db_file:
         repo_config = yaml.safe_load(db_file)
         local_repo_path = repo_config['templates_local']
 
-    depfile = os.path.join(local_repo_path, 'dependencies.yml')
-    if not os.path.isfile(depfile):
-        raise RepoStructureException("File dependencies.yml not found in template repo")
-    try:
-        with open(depfile, 'r') as f:
-            dependencies = yaml.safe_load(f)
-    except Exception as e:
-        logger.exception(
-            "Could not parse dependencies.yml in template repo: {}".format(str(e)))
-        raise RepoStructureException(
-            "Could not parse dependencies.yml in template repo: {}".format(str(e)))
+    # loop through OS/platform types
+    for platform in os.listdir(local_repo_path):
+        path = os.path.join(local_repo_path, platform)
+        if not os.path.isdir(path) or platform.startswith('.'):
+            continue
 
-    unsynced_devtype = []
-    devtype: DeviceType
-    for devtype in DeviceType:
-        if devtype.name in dependencies:
-            update_required = False
-            for dependency in dependencies[devtype.name]:
-                if dependency in updated_templates:
-                    update_required = True
-            if update_required:
-                logger.info("Template for device type {} has been updated".
-                            format(devtype.name))
-                unsynced_devtype.append(devtype)
+        mapfile = os.path.join(path, 'mapping.yml')
+        if not os.path.isfile(mapfile):
+            raise RepoStructureException(
+                "File mapping.yml not found in template repo {}".format(path))
+        try:
+            with open(mapfile, 'r') as f:
+                mapping = yaml.safe_load(f)
+        except Exception as e:
+            logger.exception(
+                "Could not parse {}/mapping.yml in template repo: {}".format(path, str(e)))
+            raise RepoStructureException(
+                "Could not parse {}/mapping.yml in template repo: {}".format(path, str(e)))
+
+        devtype: DeviceType
+        for devtype in DeviceType:
+            if devtype.name in mapping:
+                update_required = False
+                try:
+                    dependencies = list([mapping[devtype.name]['entrypoint']])
+                    if 'dependencies' in mapping[devtype.name] and \
+                            isinstance(mapping[devtype.name]['dependencies'], list):
+                        dependencies.extend(mapping[devtype.name]['dependencies'])
+                except KeyError as e:
+                    logger.exception(
+                        "Could not parse mapping.yml in template repo for {}, value not found: {}".
+                        format(devtype.name, str(e)))
+                    raise RepoStructureException(
+                        "Could not parse mapping.yml in template repo for {}, value not found: {}".
+                        format(devtype.name, str(e)))
+
+                for dependency in dependencies:
+                    if os.path.join(platform, dependency) in updated_templates:
+                        update_required = True
+                if update_required:
+                    logger.info("Template for device type {} has been updated".
+                                format(devtype.name))
+                    unsynced_devtype.add((devtype, platform))
 
     return unsynced_devtype

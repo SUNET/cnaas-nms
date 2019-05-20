f91ce162326d7954a5fbb4bb4673c47c1da55f4a
Kristofer Hallin <kristofer@sunet.se>
2019-05-20 14:43:21 +0200

    * Removed leftovers from merges

--- src/cnaas_nms/api/__init__.py
+++ src/cnaas_nms/api/__init__.py
@@ -8,12 +8,8 @@ from cnaas_nms.api.interface import InterfaceApi
 from cnaas_nms.api.mgmtdomain import MgmtdomainsApi, MgmtdomainByIdApi
 from cnaas_nms.api.jobs import JobsApi
 from cnaas_nms.api.repository import RepositoryApi
-
 from cnaas_nms.api.settings import SettingsApi
 from cnaas_nms.api.groups import GroupsApi
-from cnaas_nms.api.groups import GroupsApiByName
-from cnaas_nms.api.query import QueryApi
-from cnaas_nms.api.groups import GroupsApi, GroupsApiById, DeviceGroupsApi
 from cnaas_nms.api.groups import GroupsApi, GroupsApiById, DeviceGroupsApi, DeviceGroupsApiById
 
 
@@ -44,12 +40,8 @@ api.add_resource(JobsApi, f'/api/{ API_VERSION }/job')
 # File repository
 api.add_resource(RepositoryApi, f'/api/{ API_VERSION }/repository/<string:repo>')
 
-# Query
-api.add_resource(QueryApi, f'/api/{ API_VERSION }/query')
-
 # Settings
-api.add_resource(RepositoryApi, '/api/v1.0/repository/<string:repo>')
-api.add_resource(SettingsApi, '/api/v1.0/settings')
+api.add_resource(SettingsApi, f'/api/{ API_VERSION }/settings')
 
 # Groups
 api.add_resource(GroupsApi, f'/api/{ API_VERSION }/groups')

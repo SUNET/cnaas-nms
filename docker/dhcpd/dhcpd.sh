5de21343810e75fefe735cc7590cb48e9a5334c3
Johan Marcusson <johan.marcusson@sunet.se>
2019-05-21 09:59:17 +0200

    update docker to use configurable git repos set by env variables

--- docker/dhcpd/dhcpd.sh
+++ docker/dhcpd/dhcpd.sh
@@ -7,6 +7,20 @@ sed -e "s/^\(password: \).\+$/\1 $DB_PASSWORD/" \
   < /etc/cnaas-nms/db_config.yml > db_config.yml.new \
   && mv -f db_config.yml.new /etc/cnaas-nms/db_config.yml
 
+if [ -z "$GITREPO_ETC" ]
+then
+	cd /opt/cnaas
+	rm -rf /opt/cnaas/etc
+	git clone "$GITREPO_ETC" etc
+	if [ -f "/opt/cnaas/etc/dhcpd/dhcpd.conf" ]
+	then
+		cp /opt/cnaas/etc/dhcpd/dhcpd.conf /opt/cnaas/dhcpd.conf
+	fi
+fi
+
+cd /opt/cnaas/venv/cnaas-nms
+git pull
+
 touch /var/lib/dhcp/dhcpd.leases
 
 /usr/sbin/dhcpd -4 -f -d --no-pid -cf /opt/cnaas/dhcpd.conf

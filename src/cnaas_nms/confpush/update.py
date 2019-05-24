5590f418671ae0a906bec66bd708dfcd9a6b0716
Johan Marcusson <johan.marcusson@sunet.se>
2019-05-24 09:12:06 +0200

    Allow update_interfacedb to replace and delete entries

--- src/cnaas_nms/confpush/update.py
+++ src/cnaas_nms/confpush/update.py
@@ -10,8 +10,11 @@ from cnaas_nms.tools.log import get_logger
 logger = get_logger()
 
 
-def update_interfacedb(hostname: str) -> Optional[List[dict]]:
+def update_interfacedb(hostname: str, replace: bool = False, delete: bool = False) \
+        -> Optional[List[dict]]:
     """Update interface DB with any new physical interfaces for specified device.
+    If replace is set, any existing records in the database will get overwritten.
+    If delete is set, all entries in database for this device will be removed.
 
     Returns:
         List of interfaces that was added to DB
@@ -31,25 +34,39 @@ def update_interfacedb(hostname: str) -> Optional[List[dict]]:
         uplinks, neighbor_hostnames = get_uplinks(session, hostname)
         uplinks_ifnames = [x['ifname'] for x in uplinks]
         phy_interfaces = filter_interfaces(iflist, platform=dev.platform, include='physical')
-        existing_ifs = get_interfacedb_ifs(session, hostname)
+#        existing_ifs = get_interfacedb_ifs(session, hostname)
 
         updated = False
-        for intf in phy_interfaces:
-            if intf in existing_ifs:
+        for intf_name in phy_interfaces:
+            intf: Interface = session.query(Interface).filter(Interface.device == dev).\
+                filter(Interface.name == intf_name).one_or_none()
+            if intf:
+                new_intf = False
+            else:
+                new_intf = True
+                intf: Interface = Interface()
+            if not new_intf and delete:
+                logger.debug("Deleting interface {} on device {} from interface DB".format(
+                    intf_name, dev.hostname
+                ))
+                session.delete(intf)
+                continue
+            elif not new_intf and not replace:
                 continue
             updated = True
-            logger.debug("New physical interface found on device {}: {}".format(
-                dev.hostname, intf
+            logger.debug("New/updated physical interface found on device {}: {}".format(
+                dev.hostname, intf_name
             ))
-            new_intf: Interface = Interface()
-            if intf in uplinks_ifnames:
-                new_intf.configtype = InterfaceConfigType.ACCESS_UPLINK
+            if intf_name in uplinks_ifnames:
+                intf.configtype = InterfaceConfigType.ACCESS_UPLINK
+                intf.data = {'neighbor': neighbor_hostnames[uplinks_ifnames.index(intf_name)]}
             else:
-                new_intf.configtype = InterfaceConfigType.ACCESS_AUTO
-            new_intf.name = intf
-            new_intf.device = dev
-            session.add(new_intf)
-            ret.append(new_intf.as_dict())
+                intf.configtype = InterfaceConfigType.ACCESS_AUTO
+            intf.name = intf_name
+            intf.device = dev
+            if new_intf:
+                session.add(intf)
+            ret.append(intf.as_dict())
         if updated:
             dev.synchronized = False
     return ret

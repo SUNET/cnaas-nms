811ae3fb0093296515ef93de56a4b75b3b851168
Johan Marcusson <johan.marcusson@sunet.se>
2019-06-11 09:55:28 +0200

    Expand integration test to start ztp

--- test/integrationtests.py
+++ test/integrationtests.py
@@ -2,10 +2,11 @@
 
 import requests
 import time
+import unittest
 
 
-URL="https://localhost"
-TLS_VERIFY=False
+URL = "https://localhost"
+TLS_VERIFY = False
 
 
 if not TLS_VERIFY:
@@ -13,62 +14,88 @@ if not TLS_VERIFY:
     urllib3.disable_warnings()
 
 
-def wait_connect():
-    for i in range(100):
-        try:
-            r = requests.get(
-                f'{URL}/api/v1.0/device',
-                verify=TLS_VERIFY
-            )
-        except Exception as e:
-            print("Exception {}, retrying in 1 second".format(str(e)))
-            time.sleep(1)
-        else:
-            if r.status_code == 200:
-                return True
-            else:
-                print("Bad status code {}, retrying in 1 second...".format(r.status_code))
-                time.sleep(1)
-    return False
+class GetTests(unittest.TestCase):
+    def setUp(self):
+        self.assertTrue(self.wait_connect(), "Connection to API failed")
 
-
-def wait_for_discovered_device():
-    for i in range(100):
-        r = requests.get(
-            f'{URL}/api/v1.0/device',
-            params={'filter': 'state,DISCOVERED'},
+        r = requests.put(
+            f'{URL}/api/v1.0/repository/templates',
+            json={"action": "refresh"},
             verify=TLS_VERIFY
         )
-        if len(r.json()['data']['devices']) == 1:
-            return r.json()['data']['devices'][0]['hostname']
-        else:
-            time.sleep(1)
-    return False
+        print("Template refresh status: {}".format(r.status_code))
+        self.assertEqual(r.status_code, 200, "Failed to refresh templates")
+        r = requests.put(
+            f'{URL}/api/v1.0/repository/settings',
+            json={"action": "refresh"},
+            verify=TLS_VERIFY
+        )
+        print("Settings refresh status: {}".format(r.status_code))
+        self.assertEqual(r.status_code, 200, "Failed to refresh settings")
+
+    def wait_connect(self):
+        for i in range(100):
+            try:
+                r = requests.get(
+                    f'{URL}/api/v1.0/device',
+                    verify=TLS_VERIFY
+                )
+            except Exception as e:
+                print("Exception {}, retrying in 1 second".format(str(e)))
+                time.sleep(1)
+            else:
+                if r.status_code == 200:
+                    return True
+                else:
+                    print("Bad status code {}, retrying in 1 second...".format(r.status_code))
+                    time.sleep(1)
+        return False
+
+    def wait_for_discovered_device(self):
+        for i in range(100):
+            r = requests.get(
+                f'{URL}/api/v1.0/device',
+                params={'filter': 'state,DISCOVERED'},
+                verify=TLS_VERIFY
+            )
+            if len(r.json()['data']['devices']) == 1:
+                return r.json()['data']['devices'][0]['hostname'],\
+                       r.json()['data']['devices'][0]['id']
+            else:
+                time.sleep(1)
+        return None, None
+
+    def check_jobid(self, job_id):
+        for i in range(100):
+            r = requests.get(
+                f'{URL}/api/v1.0/job/{job_id}',
+                verify=TLS_VERIFY
+            )
+            if r.status_code == 200:
+                if r.json()['data']['jobs'][0]['status'] == 'FINISHED':
+                    return r.json()['data']['jobs'][0]['result']
+                else:
+                    time.sleep(1)
+                    continue
+            else:
+                raise Exception
+
+    def test_ztp(self):
+        hostname, device_id = self.wait_for_discovered_device()
+        print("Discovered hostname, id: {}, {}".format(hostname, device_id))
+        self.assertTrue(hostname, "No device in state discovered found for ZTP")
+        r = requests.post(
+            f'{URL}/api/v1.0/device_init/{device_id}',
+            json={"hostname": "eosaccess", "device_type": "ACCESS"},
+            verify=TLS_VERIFY
+        )
+        self.assertEqual(r.status_code, 200, "Failed to start device_init")
+        self.assertEqual(r.json()['status'], 'success', "Failed to start device_init")
+        job_id = r.json()['job_id']
+        result = self.check_jobid(job_id)
+        self.assertTrue(result['eosaccess'][0]['failed'],
+                        "Expected failed result since mgmt_ip changed")
 
 
-def main():
-    if wait_connect():
-        print("Success")
-    else:
-        print("Fail")
-        return
-
-    r = requests.put(
-        f'{URL}/api/v1.0/repository/templates',
-        json={"action": "refresh"},
-        verify=TLS_VERIFY
-    )
-    print("Template refresh status: {}".format(r.status_code))
-    r = requests.put(
-        f'{URL}/api/v1.0/repository/settings',
-        json={"action": "refresh"},
-        verify=TLS_VERIFY
-    )
-    print("Settings refresh status: {}".format(r.status_code))
-    discovered_hostname = wait_for_discovered_device()
-    print("Discovered hostname: {}".format(discovered_hostname))
-
-
-if __name__ == "__main__":
-    main()
-
+if __name__ == '__main__':
+    unittest.main()

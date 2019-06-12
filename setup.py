ae9a8ec1a50133e196b3cd123a02bd75cfb25e93
Kristofer Hallin <kristofer@sunet.se>
2019-06-12 11:06:03 +0200

    Include requirements and version

--- setup.py
+++ setup.py
@@ -1,21 +1,31 @@
 import os
 import setuptools
 
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+version = '0.1.0'
+
+requires = open(os.path.join(here, 'requirements.txt'), 'r').read().split()
+
+
 setuptools.setup(
     name='cnaas_nms',
-    version="0.1.0",
-    author="Johan Marcusson, Kristofer Hallin",
-    author_email="johan@sunet.se, kristofer@sunet.se",
-    description="Campus Network as-a-Service - Network Management System (Campus network automation software)",
-    # long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/sunet/cnaas-nms",
+    author='SUNET',
+    author_email='',
+    version=version,
+    description='Campus Network as-a-Service - Network Management System (Campus network automation software)',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/sunet/cnaas-nms',
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: Copyright (c) 2019, SUNET (BSD 2-clause license)",
-        "Operating System :: UNIX/Linux",
+        'Programming Language :: Python :: 3',
+        'License :: Copyright (c) 2019, SUNET (BSD 2-clause license)',
+        'Operating System :: UNIX/Linux',
     ],
     package_dir={'': 'src'},
-    packages=setuptools.find_packages("src", exclude=["tests"]),
+    packages=setuptools.find_packages('src', exclude=['tests']),
     include_package_data=True,
+    install_requires=requires,
 )

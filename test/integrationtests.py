75e13b70e268f3e5519101aa7a469c7b2ba26542
Johan Marcusson <johan.marcusson@sunet.se>
2019-08-21 13:14:14 +0200

    Change interface before syncing access device to make sure auto_push does something

--- test/integrationtests.py
+++ test/integrationtests.py
@@ -102,15 +102,7 @@ class GetTests(unittest.TestCase):
         self.assertFalse(result_step2['eosaccess'][0]['failed'],
                          "Could not reach device after ZTP")
 
-    def test_2_syncto_access(self):
-        r = requests.post(
-            f'{URL}/api/v1.0/device_syncto',
-            json={"hostname": "eosaccess", "dry_run": True, "auto_push": True},
-            verify=TLS_VERIFY
-        )
-        self.assertEqual(r.status_code, 200, "Failed to do sync_to access")
-
-    def test_3_interfaces(self):
+    def test_2_interfaces(self):
         r = requests.get(
             f'{URL}/api/v1.0/device/eosaccess/interfaces',
             verify=TLS_VERIFY
@@ -123,6 +115,14 @@ class GetTests(unittest.TestCase):
             verify=TLS_VERIFY
         )
         self.assertEqual(r.status_code, 200, "Failed to update interface")
+
+    def test_3_syncto_access(self):
+        r = requests.post(
+            f'{URL}/api/v1.0/device_syncto',
+            json={"hostname": "eosaccess", "dry_run": True, "auto_push": True},
+            verify=TLS_VERIFY
+        )
+        self.assertEqual(r.status_code, 200, "Failed to do sync_to access")
 
     def test_4_syncto_dist(self):
         r = requests.post(

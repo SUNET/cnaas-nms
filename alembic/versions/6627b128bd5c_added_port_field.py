cd1361e58a8df42de42ac88bc1d5b260b408c0ff
Johan Marcusson <johan.marcusson@sunet.se>
2019-08-30 10:19:38 +0200

    Fix alembic revisions for merge

--- alembic/versions/6627b128bd5c_added_port_field.py
+++ alembic/versions/6627b128bd5c_added_port_field.py
@@ -11,7 +11,8 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '6627b128bd5c'
-down_revision = '922589188efe'
+#down_revision = '922589188efe'
+down_revision = '6c6bec879fa8'
 branch_labels = None
 depends_on = None
 

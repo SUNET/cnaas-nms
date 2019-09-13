caa839a57bbea979e62653371d2cb555a6c21889
Johan Marcusson <johan.marcusson@sunet.se>
2019-09-13 09:09:21 +0200

    Handle one more exception type for non-existing repo

--- src/cnaas_nms/db/git.py
+++ src/cnaas_nms/db/git.py
@@ -143,7 +143,8 @@ def _refresh_repo_task(repo_type: RepoType = RepoType.TEMPLATES) -> str:
                     name_only=True).split()
             changed_files.update(diff_files)
             prev_commit = item.commit.hexsha
-    except (InvalidGitRepositoryError, NoSuchPathError) as e:
+    # "ValueError: Reference at 'refs/heads/master' does not exist" can also cause this
+    except (InvalidGitRepositoryError, NoSuchPathError, ValueError) as e:
         logger.info("Local repository {} not found, cloning from remote".\
                     format(local_repo_path))
         try:

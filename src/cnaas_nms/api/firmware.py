c91a9681e114f62ce726c9ab2548dd47b85fafda
Kristofer Hallin <kristofer@sunet.se>
2019-09-20 13:32:59 +0200

    Better error handling.

--- src/cnaas_nms/api/firmware.py
+++ src/cnaas_nms/api/firmware.py
@@ -1,29 +1,36 @@
 import os
 import json
 import requests
+import yaml
 
 from flask import request
 from flask_restful import Resource
 from cnaas_nms.scheduler.scheduler import Scheduler
 from cnaas_nms.api.generic import empty_result
 from cnaas_nms.scheduler.wrapper import job_wrapper
+from cnaas_nms.tools.log import get_logger
+from cnaas_nms.tools.get_apidata import get_apidata
+
+logger = get_logger()
 
 
-if 'HTTPD_HOSTNAME' not in os.environ:
-    URL = 'https://cnaas_httpd/api/v1.0/firmware'
+apidata = get_apidata()
+if isinstance(apidata, dict) and 'httpd_url' in apidata:
+    HTTPD_URL = apidata['httpd_url']
 else:
-    URL = os.environ['HTTPD_HOSTNAME']
+    HTTPD_URL = 'https://cnaas_httpd/api/v1.0/'
 
 
 @job_wrapper
 def get_firmware(**kwargs: dict) -> str:
     try:
-        res = requests.post(URL, json=kwargs,
+        res = requests.post(HTTPD_URL, json=kwargs,
                             verify=False)
         json_data = json.loads(res.content)
-    except Exception:
-        return 'Could not parse result from firmware fetch'
-    if 'message' in json_data:
+    except Exception as e:
+        logger.exception(f"Exception while getting firmware: {e}")
+        return 'Could not download firmware'
+    if json_data['status'] == 'error':
         return json_data['message']
     return 'File downloaded from: ' + kwargs['url']
 
@@ -31,21 +38,25 @@ def get_firmware(**kwargs: dict) -> str:
 @job_wrapper
 def get_firmware_chksum(**kwargs: dict) -> str:
     try:
-        url = URL + '/' + kwargs['filename']
+        url = HTTPD_URL + '/' + kwargs['filename']
         res = requests.get(url, verify=False)
         json_data = json.loads(res.content)
-    except Exception:
-        return 'Failed to get file'
+    except Exception as e:
+        logger.exception(f"Exceptionb while getting checksum: {e}")
+        return 'Failed to get checksum for ' + kwargs['filename']
+    if json_data['status'] == 'error':
+        return json_data['message']
     return json_data['data']['file']['sha1']
 
 
 @job_wrapper
 def remove_file(**kwargs: dict) -> str:
     try:
-        url = URL + '/' + kwargs['filename']
+        url = HTTPD_URL + '/' + kwargs['filename']
         res = requests.delete(url, verify=False)
         json_data = json.loads(res.content)
-    except Exception:
+    except Exception as e:
+        logger.exception(f"Exception when removing firmware: {e}")
         return 'Failed to remove file'
     if json_data['status'] == 'error':
         return 'Failed to remove file ' + kwargs['filename']
@@ -70,7 +81,8 @@ class FirmwareApi(Resource):
             res = requests.get('https://localhost/api/v1.0/firmware',
                                verify=False)
             json_data = json.loads(res.content)
-        except Exception:
+        except Exception as e:
+            logger.exception(f"Exception when getting images: {e}")
             return empty_result(status='error',
                                 data='Could not get files'), 404
         return empty_result(status='success', data=json_data)

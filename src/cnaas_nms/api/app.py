38243a133a794b0a41f83508c8c225c129e2c51b
Kristofer Hallin <kristofer@sunet.se>
2019-10-02 12:16:47 +0200

    Set the first half of the API path as a prefix when creating the API instance. A little less cluttered code.

--- src/cnaas_nms/api/app.py
+++ src/cnaas_nms/api/app.py
@@ -18,47 +18,47 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(128)
 
-api = Api(app)
+api = Api(app, prefix=f'/api/{ __api_version__ }')
 
 
 # Devices
-api.add_resource(DeviceByIdApi, f'/api/{ __api_version__ }/device/<int:device_id>')
-api.add_resource(DeviceInitApi, f'/api/{ __api_version__ }/device_init/<int:device_id>')
-api.add_resource(DeviceDiscoverApi, f'/api/{ __api_version__ }/device_discover')
-api.add_resource(DeviceSyncApi, f'/api/{ __api_version__ }/device_syncto')
-api.add_resource(DeviceConfigApi, f'/api/{ __api_version__ }/device/<string:hostname>/generate_config')
-api.add_resource(DeviceApi, f'/api/{ __api_version__ }/device')
-api.add_resource(DevicesApi, f'/api/{ __api_version__ }/devices')
+api.add_resource(DeviceByIdApi, '/device/<int:device_id>')
+api.add_resource(DeviceInitApi, '/device_init/<int:device_id>')
+api.add_resource(DeviceDiscoverApi, '/device_discover')
+api.add_resource(DeviceSyncApi, '/device_syncto')
+api.add_resource(DeviceConfigApi, '/device/<string:hostname>/generate_config')
+api.add_resource(DeviceApi, '/device')
+api.add_resource(DevicesApi, '/devices')
 # device/<string:hostname>/current_config
 
 # Links
-api.add_resource(LinknetsApi, f'/api/{ __api_version__ }/linknets')
+api.add_resource(LinknetsApi, '/linknets')
 
 # Interfaces
-api.add_resource(InterfaceApi, f'/api/{ __api_version__ }/device/<string:hostname>/interfaces')
+api.add_resource(InterfaceApi, '/device/<string:hostname>/interfaces')
 
 # Management domains
-api.add_resource(MgmtdomainsApi, f'/api/{ __api_version__ }/mgmtdomains')
-api.add_resource(MgmtdomainByIdApi, f'/api/{ __api_version__ }/mgmtdomain/<int:mgmtdomain_id>')
+api.add_resource(MgmtdomainsApi, '/mgmtdomains')
+api.add_resource(MgmtdomainByIdApi, '/mgmtdomain/<int:mgmtdomain_id>')
 
 # Jobs
-api.add_resource(JobsApi, f'/api/{ __api_version__ }/jobs')
-api.add_resource(JobByIdApi, f'/api/{ __api_version__ }/job/<string:id>')
-api.add_resource(JobLockApi, f'/api/{ __api_version__ }/joblocks')
+api.add_resource(JobsApi, '/jobs')
+api.add_resource(JobByIdApi, '/job/<string:id>')
+api.add_resource(JobLockApi, '/joblocks')
 
 # File repository
-api.add_resource(RepositoryApi, f'/api/{ __api_version__ }/repository/<string:repo>')
+api.add_resource(RepositoryApi, '/repository/<string:repo>')
 
 # Firmware
-api.add_resource(FirmwareApi, f'/api/{ __api_version__ }/firmware')
-api.add_resource(FirmwareImageApi, f'/api/{ __api_version__ }/firmware/<string:filename>')
+api.add_resource(FirmwareApi, '/firmware')
+api.add_resource(FirmwareImageApi, '/firmware/<string:filename>')
 
 # Settings
-api.add_resource(SettingsApi, f'/api/{ __api_version__ }/settings')
+api.add_resource(SettingsApi, '/settings')
 
 # Groups
-api.add_resource(GroupsApi, f'/api/{ __api_version__ }/groups')
-api.add_resource(GroupsApiById, f'/api/{ __api_version__ }/groups/<string:group_name>')
+api.add_resource(GroupsApi, '/groups')
+api.add_resource(GroupsApiById, '/groups/<string:group_name>')
 
 # Plugins
-api.add_resource(PluginsApi, f'/api/{ __api_version__ }/plugins')
+api.add_resource(PluginsApi, '/plugins')

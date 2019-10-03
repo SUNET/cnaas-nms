8399e0f3af3f549d2f81018adfb4fb03f92233b6
Johan Marcusson <johan.marcusson@sunet.se>
2019-10-03 13:06:38 +0200

    Move gevent monkey patch so it's only done if we need it, ie when we run the flask webserver in standalone mode with debugging/auto-reloader enabled

--- src/cnaas_nms/run.py
+++ src/cnaas_nms/run.py
@@ -1,19 +1,11 @@
 import os
-import yaml
 import coverage
 import atexit
 import signal
 
-from cnaas_nms.api import app
-from cnaas_nms.scheduler.scheduler import Scheduler
-from cnaas_nms.plugins.pluginmanager import PluginManagerHandler
-from cnaas_nms.db.session import sqla_session
-from cnaas_nms.db.joblock import Joblock
-from cnaas_nms.tools.log import get_logger
 from cnaas_nms.tools.get_apidata import get_apidata
+# Do late imports for anything cnaas/flask related so we can do gevent monkey patch, see below
 
-
-logger = get_logger()
 
 os.environ['PYTHONPATH'] = os.getcwd()
 
@@ -34,6 +26,10 @@ if 'COVERAGE' in os.environ:
 
 
 def get_app():
+    from cnaas_nms.scheduler.scheduler import Scheduler
+    from cnaas_nms.plugins.pluginmanager import PluginManagerHandler
+    from cnaas_nms.db.session import sqla_session
+    from cnaas_nms.db.joblock import Joblock
     # If running inside uwsgi, a separate "mule" will run the scheduler
     try:
         import uwsgi
@@ -49,16 +45,23 @@ def get_app():
         with sqla_session() as session:
             Joblock.clear_locks(session)
     except Exception as e:
-        logger.exception("Unable to clear old locks from database at startup: {}".format(str(e)))
+        print("Unable to clear old locks from database at startup: {}".format(str(e)))
 
     return app.app
 
 
 if __name__ == '__main__':
+    # gevent monkey patching required if you start flask with the auto-reloader (debug mode)
+    from gevent import monkey
+    monkey.patch_all()
+    from cnaas_nms.api import app
+
     apidata = get_apidata()
     if isinstance(apidata, dict) and 'host' in apidata:
         app.socketio.run(get_app(), debug=True, host=apidata['host'])
     else:
         app.socketio.run(get_app(), debug=True)
 else:
+    from cnaas_nms.api import app
+
     cnaas_app = get_app()

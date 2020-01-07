28e5f1f47c287dec47bd44e72cae4e7e83226ad9
Johan Marcusson <johan.marcusson@sunet.se>
2020-01-07 15:38:13 +0100

    Make sync_devices log jobid on log entries

--- src/cnaas_nms/confpush/sync_devices.py
+++ src/cnaas_nms/confpush/sync_devices.py
@@ -23,12 +23,12 @@ from cnaas_nms.db.joblock import Joblock, JoblockError
 from cnaas_nms.db.git import RepoStructureException
 from cnaas_nms.confpush.nornir_helper import NornirJobResult
 from cnaas_nms.scheduler.wrapper import job_wrapper
+from cnaas_nms.scheduler.thread_data import set_thread_data
 
 from cnaas_nms.scheduler.scheduler import Scheduler
 from nornir.plugins.tasks.networking import napalm_get
 
 
-logger = get_logger()
 AUTOPUSH_MAX_SCORE = 10
 PRIVATE_ASN_START = 4200000000
 
@@ -40,6 +40,7 @@ def generate_asn(ipv4_address: IPv4Address) -> Optional[int]:
 
 
 def get_evpn_spines(session, settings: dict):
+    logger = get_logger()
     device_hostnames = []
     for entry in settings['evpn_spines']:
         if 'hostname' in entry and Device.valid_hostname(entry['hostname']):
@@ -55,6 +56,7 @@ def get_evpn_spines(session, settings: dict):
 
 
 def resolve_vlanid(vlan_name: str, vxlans: dict) -> Optional[int]:
+    logger = get_logger()
     if type(vlan_name) == int:
         return int(vlan_name)
     if not isinstance(vlan_name, str):
@@ -94,6 +96,8 @@ def push_sync_device(task, dry_run: bool = True, generate_only: bool = False,
     Returns:
 
     """
+    set_thread_data(job_id)
+    logger = get_logger()
     hostname = task.host.name
     with sqla_session() as session:
         dev: Device = session.query(Device).filter(Device.hostname == hostname).one()
@@ -299,6 +303,7 @@ def generate_only(hostname: str) -> (str, dict):
     Returns:
         (string with config, dict with available template variables)
     """
+    logger = get_logger()
     nr = cnaas_nms.confpush.nornir_helper.cnaas_init()
     nr_filtered = nr.filter(name=hostname).filter(managed=True)
     template_vars = {}
@@ -350,6 +355,7 @@ def sync_check_hash(task, force=False, dry_run=True):
 
 
 def update_config_hash(task):
+    logger = get_logger()
     try:
         res = task.run(task=napalm_get, getters=["config"])
         if not isinstance(res, MultiResult) or len(res) != 1 or not isinstance(res[0].result, dict) \
@@ -390,6 +396,7 @@ def sync_devices(hostname: Optional[str] = None, device_type: Optional[str] = No
     Returns:
         NornirJobResult
     """
+    logger = get_logger()
     nr = cnaas_nms.confpush.nornir_helper.cnaas_init()
     if hostname:
         nr_filtered = nr.filter(name=hostname).filter(managed=True)

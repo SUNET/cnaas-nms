75b28341c438a66022b15b06370f2bc91ae1d6b9
Johan Marcusson <johan.marcusson@sunet.se>
2020-01-21 12:38:22 +0100

    Add /system/version API call

--- src/cnaas_nms/api/system.py
+++ src/cnaas_nms/api/system.py
@@ -1,9 +1,12 @@
-from flask import request
-from flask_restplus import Resource, Namespace, fields
+from os.path import dirname
+from flask_restplus import Resource, Namespace
 from flask_jwt_extended import jwt_required
+from git import Repo
+from git import InvalidGitRepositoryError, NoSuchPathError
 
 from cnaas_nms.api.generic import empty_result
 from cnaas_nms.api import app
+import cnaas_nms.version
 from cnaas_nms.version import __api_version__
 
 
@@ -19,4 +22,27 @@ class ShutdownApi(Resource):
         exit()
 
 
+class VersionApi(Resource):
+    def get(self):
+        version_str = cnaas_nms.version.__version__
+        try:
+            src_repo_path = dirname(dirname(dirname(cnaas_nms.version.__file__)))
+            print(src_repo_path)
+            local_repo = Repo(src_repo_path)
+            git_version_str = 'Git commit {} ({})'.format(
+                local_repo.head.commit.name_rev,
+                local_repo.head.commit.committed_datetime
+            )
+        except (InvalidGitRepositoryError, NoSuchPathError) as e:
+            git_version_str = 'No git repo found'
+        except Exception:
+            git_version_str = 'Unhandled exception'
+
+        return empty_result(status='success', data={
+            "version": version_str,
+            "git_version": git_version_str
+        })
+
+
 api.add_resource(ShutdownApi, '/shutdown')
+api.add_resource(VersionApi, '/version')

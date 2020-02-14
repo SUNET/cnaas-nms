976eee0910254d52beddf6d4d0af1851b9002c29
Johan Marcusson <johan.marcusson@sunet.se>
2020-02-14 15:36:16 +0100

    Add port bounce method via napalm config merge, including pre checks.

---
+++ src/cnaas_nms/confpush/interface_state.py
@@ -0,0 +1,118 @@
+from typing import List
+
+import yaml
+from nornir.plugins.tasks.networking import napalm_get, napalm_configure
+from nornir.plugins.tasks.text import template_file
+
+import cnaas_nms.confpush.nornir_helper
+from cnaas_nms.db.device import Device, DeviceState, DeviceType
+from cnaas_nms.db.interface import Interface, InterfaceConfigType
+from cnaas_nms.db.session import sqla_session
+from cnaas_nms.tools.log import get_logger
+
+
+def get_interface_states(hostname) -> dict:
+    logger = get_logger()
+
+    nr = cnaas_nms.confpush.nornir_helper.cnaas_init()
+    nr_filtered = nr.filter(name=hostname).filter(managed=True)
+    if len(nr_filtered.inventory) != 1:
+        raise ValueError(f"Hostname {hostname} not found in inventory")
+    nrresult = nr_filtered.run(task=napalm_get, getters=["interfaces"])
+    if not len(nrresult) == 1:
+        raise Exception(f"Could not get interfaces for {hostname}: no Nornir result")
+    if nrresult.failed or nrresult[hostname].failed:
+        raise Exception("Could not get interfaces for {}, NAPALM failed: {}".format(
+            hostname, nrresult[hostname].exception
+        ))
+    return nrresult[hostname][0].result['interfaces']
+
+
+def pre_bounce_check(hostname: str, interfaces: List[str]):
+    # Check1: Database state
+    with sqla_session() as session:
+        dev: Device = session.query(Device).filter(Device.hostname == hostname).one_or_none()
+        if not dev:
+            raise ValueError(f"Hostname {hostname} not found in database")
+        if dev.device_type != DeviceType.ACCESS or dev.state != DeviceState.MANAGED:
+            raise ValueError(f"Hostname {hostname} is not of type ACCESS or not in state MANAGED")
+        db_intfs: List = session.query(Interface).filter(Interface.device == dev).\
+            filter(Interface.configtype == InterfaceConfigType.ACCESS_UPLINK).all()
+        uplink_intf_names = [x.name for x in db_intfs]
+        for interface in interfaces:
+            if interface in uplink_intf_names:
+                raise ValueError("Can't bounce UPLINK port {} for device {}".format(
+                    interface, hostname
+                ))
+    # Check2: Current interface state on device
+    intf_states = get_interface_states(hostname)
+    for interface in interfaces:
+        if interface not in intf_states.keys():
+            raise ValueError("Specified interface {} not found on device {}".format(
+                interface, hostname
+            ))
+        if 'is_enabled' not in intf_states[interface] or not intf_states[interface]['is_enabled']:
+            raise ValueError("Specified interface {} on device {} is not enabled".format(
+                interface, hostname
+            ))
+    # Check3: config hash?
+
+
+def bounce_task(task, hostname: str, interfaces: List[str]):
+    template_vars = {'interfaces': interfaces}
+    with sqla_session() as session:
+        dev: Device = session.query(Device).filter(Device.hostname == hostname).one_or_none()
+        if not dev:
+            raise ValueError(f"Hostname {hostname} not found in database")
+        platform = dev.platform
+    with open('/etc/cnaas-nms/repository.yml', 'r') as db_file:
+        repo_config = yaml.safe_load(db_file)
+        local_repo_path = repo_config['templates_local']
+    r = task.run(
+        task=template_file,
+        name="Generate port bounce down config",
+        template="bounce-down.j2",
+        path=f"{local_repo_path}/{task.host.platform}",
+        **template_vars
+    )
+    task.host["config"] = r.result
+    task.run(
+        task=napalm_configure,
+        name="Port bounce down",
+        replace=False,
+        configuration=task.host["config"],
+    )
+    r = task.run(
+        task=template_file,
+        name="Generate port bounce up config",
+        template="bounce-up.j2",
+        path=f"{local_repo_path}/{task.host.platform}",
+        **template_vars
+    )
+    task.host["config"] = r.result
+    task.run(
+        task=napalm_configure,
+        name="Port bounce up",
+        replace=False,
+        configuration=task.host["config"],
+    )
+
+
+def bounce_interfaces(hostname: str, interfaces: List[str]) -> bool:
+    """Returns true if the device changed config down and then up.
+    Returns false if config did not change, and raises Exception if an
+    error was encountered."""
+    pre_bounce_check(hostname, interfaces)
+    nr = cnaas_nms.confpush.nornir_helper.cnaas_init()
+    nr_filtered = nr.filter(name=hostname).filter(managed=True)
+    if len(nr_filtered.inventory) != 1:
+        raise ValueError(f"Hostname {hostname} not found in inventory")
+    nrresult = nr_filtered.run(task=bounce_task, hostname=hostname, interfaces=interfaces)
+    # 5 results: bounce_task, gen down config, push down config, gen up config, push up config
+    if not len(nrresult) == 5:
+        raise Exception("Not all steps of port bounce completed")
+    if nrresult[hostname][2].changed and nrresult[hostname][4].changed:
+        return True
+    else:
+        return False
+

cf75cdffa27c933470eff14b56ae5cb0fafd7c9e
Johan Marcusson <johan.marcusson@sunet.se>
2021-01-04 10:08:03 +0100

    Fix some unit tests for cert

--- src/cnaas_nms/api/tests/test_api.py
+++ src/cnaas_nms/api/tests/test_api.py
@@ -22,7 +22,7 @@ class ApiTests(unittest.TestCase):
         self.client = self.app.test_client()
 
     def test_get_single_device(self):
-        hostname = "eosdist1"
+        hostname = self.testdata['managed_dist']
         result = self.client.get(
             f'/api/v1.0/devices',
             params={"filter[hostname]": hostname}
@@ -340,6 +340,18 @@ class ApiTests(unittest.TestCase):
         self.assertEqual(result.json['status'], 'success')
         self.assertGreaterEqual(len(result.json['data']['groups'][groupname]), 1,
                                 f"No devices found in group '{groupname}' os_versions")
+
+    def test_renew_cert_errors(self):
+        # Test invalid hostname
+        data = {"hostname": "...", "action": "RENEW"}
+        result = self.client.post('/api/v1.0/device_cert', json=data)
+        self.assertEqual(result.status_code, 400)
+
+        # Test invalid action
+        data = {"hostname": self.testdata['managed_dist']}
+        result = self.client.post('/api/v1.0/device_cert', json=data)
+        self.assertEqual(result.status_code, 400)
+        self.assertTrue("action" in result.json['message'], msg="Unexpected error message")
 
 
 if __name__ == '__main__':

dfc790b2ebe5f95f25b1f25f04977e20e7898b41
Johan Marcusson <johan.marcusson@sunet.se>
2021-02-16 15:37:21 +0100

    Allow specifying refs other than origin/

--- docker/api/cnaas-setup-branch.sh
+++ docker/api/cnaas-setup-branch.sh
@@ -6,6 +6,6 @@ set -x
 # Temporary for testing new branch
 if [ "$1" != "develop" ] ; then
 	cd /opt/cnaas/venv/cnaas-nms/
-	git checkout --track origin/$1
+	git checkout --track $1
 	python3 -m pip install -r requirements.txt
 fi

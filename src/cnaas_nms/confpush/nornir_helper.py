658040373f3b86423e289e31b42e1a9784af9a85
Johan Marcusson <johan.marcusson@sunet.se>
2021-05-07 10:30:20 +0200

    Try allowing include of templates relative to parent template

--- src/cnaas_nms/confpush/nornir_helper.py
+++ src/cnaas_nms/confpush/nornir_helper.py
@@ -19,11 +19,16 @@ class NornirJobResult(JobResult):
     change_score: Optional[float] = None
 
 
-cnaas_jinja_env = JinjaEnvironment(
+class RelativeJinjaEnvironment(JinjaEnvironment):
+    """Enable relative template paths"""
+    def join_path(self, template, parent):
+        return os.path.join(os.path.dirname(parent), template)
+
+
+cnaas_jinja_env = RelativeJinjaEnvironment(
     trim_blocks=True,
     lstrip_blocks=True,
-    keep_trailing_newline=True,
-    cache_size=0
+    keep_trailing_newline=True
 )
 
 

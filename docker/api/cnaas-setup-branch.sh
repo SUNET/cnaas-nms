6bbfdfcfd652e449fe2a213f695189a399fc4d7c
Johan Marcusson <johan.marcusson@sunet.se>
2021-05-18 13:24:51 +0200

    Try fixing ci build pr/ branch checkout

--- docker/api/cnaas-setup-branch.sh
+++ docker/api/cnaas-setup-branch.sh
@@ -6,6 +6,7 @@ set -x
 # Temporary for testing new branch
 if [ "$1" != "develop" ] ; then
 	cd /opt/cnaas/venv/cnaas-nms/
+	git fetch --all
 	git checkout --track origin/$1
 	python3 -m pip install -r requirements.txt
 fi

45d118ab010f373d48fc42267cba255820ba558d
Kat Hößel <kathoe@uninett.no>
2021-07-13 09:52:58 +0200

    api docker: Fix unhandled exception error in git_version
    Git version could not be determined because of file system permissions on the
    .git dir.
    
    - www-data gets ownership of all of /opt/cnaas/venv/.
    - run cnaas-setup-branch script as www-data, not root.
    - activate the venv when installing requirements via cnaas-setup-branch script
    
    Fixes #181

--- docker/api/cnaas-setup-branch.sh
+++ docker/api/cnaas-setup-branch.sh
@@ -8,5 +8,6 @@ if [ "$1" != "develop" ] ; then
 	cd /opt/cnaas/venv/cnaas-nms/
 	git fetch --all
 	git checkout --track origin/$1
+	source /opt/cnaas/venv/bin/activate
 	python3 -m pip install -r requirements.txt
 fi

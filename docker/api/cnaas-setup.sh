fe292e059b7bcf6bc4fa975e9e194b02097d9eb6
Kat Hößel <kathoe@uninett.no>
2021-07-19 14:11:39 +0200

    api docker: Allow checking out a specified branch during setup

--- docker/api/cnaas-setup.sh
+++ docker/api/cnaas-setup.sh
@@ -16,6 +16,8 @@ source bin/activate
 # Fetch the code and install dependencies
 git clone "$1" cnaas-nms
 cd cnaas-nms/
+# Checkout branch
+git checkout --track origin/"$2"
 git config --add remote.origin.fetch "+refs/pull/*/head:refs/remotes/origin/pr/*"
 python3 -m pip install -r requirements.txt
 

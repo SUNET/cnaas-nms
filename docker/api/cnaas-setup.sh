0276d0f3cb7ec6822efaab8df25559f10dcdfb56
Kat Hößel <kathoe@uninett.no>
2021-07-20 11:55:25 +0200

    api docker: Split setup script into 2 parts
    1. base-setup.sh: package install, run as root
    2. cnaas-setup.sh: git + pip commands, run as non-privileged user

--- docker/api/cnaas-setup.sh
+++ docker/api/cnaas-setup.sh
@@ -6,31 +6,6 @@ set -x
 export DEBIAN_FRONTEND noninteractive
 
 
-apt-get update && \
-    apt-get -y dist-upgrade && \
-    apt-get install -y \
-      git \
-      python3-venv \
-      python3-pip \
-      python3-yaml \
-      iputils-ping \
-      procps \
-      bind9-host \
-      netcat-openbsd \
-      net-tools \
-      curl \
-      netcat \
-      nginx \
-      supervisor \
-      libssl-dev \
-      libpq-dev \
-      libpcre2-dev \
-      libpcre3-dev \
-      uwsgi-plugin-python3 \
-    && apt-get clean
-
-pip3 install uwsgi
-
 # Start venv
 python3 -m venv /opt/cnaas/venv
 cd /opt/cnaas/venv/

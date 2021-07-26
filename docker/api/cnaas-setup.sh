a7efa50d1dfc343bc1ec227e19c2db36015fb48e
Kat Hößel <kathoe@uninett.no>
2021-07-26 17:27:31 +0200

    api docker: Introduce www-data, run fewer commands as root
    - fix some directory permissions, since www-data needs ownership of the venv
      (fixes #181)
    - disable pip cache, since www-data has no homedir

--- docker/api/cnaas-setup.sh
+++ docker/api/cnaas-setup.sh
@@ -11,10 +11,10 @@ python3 -m venv /opt/cnaas/venv
 cd /opt/cnaas/venv/
 source bin/activate
 
-/opt/cnaas/venv/bin/pip install -U pip
+/opt/cnaas/venv/bin/pip install --no-cache-dir -U pip
 
 # Fetch the code and install dependencies
 git clone $1 cnaas-nms
 cd cnaas-nms/
 git config --add remote.origin.fetch "+refs/pull/*/head:refs/remotes/origin/pr/*"
-python3 -m pip install -r requirements.txt
+python3 -m pip install --no-cache-dir -r requirements.txt

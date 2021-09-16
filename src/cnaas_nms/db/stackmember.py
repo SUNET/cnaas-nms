b105d9e22edebcd06fb1ac08c8500c05461baa8e
Kat Hößel <kathoe@uninett.no>
2021-09-16 15:29:11 +0200

    Extend data model to represent stackmembers

---
+++ src/cnaas_nms/db/stackmember.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, Unicode, UniqueConstraint, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+
+import cnaas_nms.db.base
+
+
+class Stackmember(cnaas_nms.db.base.Base):
+    __tablename__ = 'stackmember'
+    __table_args__ = (
+        UniqueConstraint('device_id', 'member_no'),
+    )
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    device_id = Column(Integer, ForeignKey('device.id'), nullable=False)
+    device = relationship("Device", foreign_keys=[device_id],
+                          backref=backref("Stackmember", cascade="all, delete-orphan"))
+    hardware_id = Column(String(64), nullable=False, unique=True)
+    member_no = Column(Integer, nullable=False)
+    priority = Column(Integer)

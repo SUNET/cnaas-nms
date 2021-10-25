f76c4a842025f1407fa7afb05b4361ca3ab2f90f
Simon Oliver Tveit <simtve@uninett.no>
2021-10-25 11:01:01 +0200

    Cleanup db device tests

--- src/cnaas_nms/db/tests/test_device.py
+++ src/cnaas_nms/db/tests/test_device.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
 import unittest
-import pkg_resources
-import yaml
-import os
-import pprint
 
 from ipaddress import IPv4Address
 
@@ -27,69 +22,41 @@ class DeviceTests(unittest.TestCase):
             device_type=DeviceType.DIST,
         )
 
-    def test_add_and_delete_dist_device(self):
-        new_device = DeviceTests.create_test_device()
-        with sqla_test_session() as session:
-            session.add(new_device)
-            instance = session.query(Device).filter(Device.hostname == 'unittest').first()
-            self.assertEquals(instance, new_device)
-            session.delete(instance)
-            deleted_instance = session.query(Device).filter(Device.hostname == 'unittest').first()
-            self.assertIsNone(deleted_instance)
-
-    def test_get_device_linknets(self):
+    def test_get_linknets(self):
         device1 = DeviceTests.create_test_device('test-device1')
         device2 = DeviceTests.create_test_device('test-device2')
         with sqla_test_session() as session:
             session.add(device1)
             session.add(device2)
-            test_linknet = Linknet(
-                device_a = device1,
-                device_b = device2
-            )
-            d = session.query(Device).filter(Device.hostname == 'test-device1').one()
-            n = session.query(Device).filter(Device.hostname == 'test-device2').one()
-            self.assertTrue(d.get_linknets(session))   # assert that list is not empty
-            self.assertTrue(n.get_linknets(session))
-            for linknet in d.get_linknets(session):
-                self.assertIsInstance(linknet, Linknet)
-            self.assertEquals([test_linknet], d.get_linknets(session))
-            self.assertEquals([test_linknet], n.get_linknets(session))
+            test_linknet = Linknet(device_a=device1, device_b=device2)
+            device1 = session.query(Device).filter(Device.hostname == 'test-device1').one()
+            device2 = session.query(Device).filter(Device.hostname == 'test-device2').one()
+            self.assertEquals([test_linknet], device1.get_linknets(session))
+            self.assertEquals([test_linknet], device2.get_linknets(session))
 
-    def test_get_device_neighbors(self):
-        new_device = DeviceTests.create_test_device('testdevice')
-        neighbour_device = DeviceTests.create_test_device('neighbourdevice')
+    def test_get_links_to(self):
+        device1 = DeviceTests.create_test_device('test-device1')
+        device2 = DeviceTests.create_test_device('test-device2')
         with sqla_test_session() as session:
-            session.add(new_device)
-            session.add(neighbour_device)
-            test_linknet = Linknet(
-                device_a = new_device,
-                device_b = neighbour_device
-            )
-            # check neighbour relation one way
-            d = session.query(Device).filter(Device.hostname == 'testdevice').one()
-            self.assertEquals([neighbour_device], d.get_neighbors(session))
-            # check the other way
-            n = session.query(Device).filter(Device.hostname == 'neighbourdevice').one()
-            self.assertEquals([new_device], n.get_neighbors(session))
-            # check type
-            for nei in d.get_neighbors(session):
-                self.assertIsInstance(nei, Device)
+            session.add(device1)
+            session.add(device2)
+            test_linknet = Linknet(device_a=device1, device_b=device2)
+            device1 = session.query(Device).filter(Device.hostname == 'test-device1').one()
+            device2 = session.query(Device).filter(Device.hostname == 'test-device2').one()
+            self.assertEquals([test_linknet], device1.get_links_to(session, device2))
+            self.assertEquals([test_linknet], device2.get_links_to(session, device1))
 
-    def test_add_stackmember(self):
+    def test_get_neighbors(self):
+        device1 = DeviceTests.create_test_device('test-device1')
+        device2 = DeviceTests.create_test_device('test-device2')
         with sqla_test_session() as session:
-            new_stack = DeviceTests.create_test_device()
-            session.add(new_stack)
-            session.flush()
-            stackmember1 = Stackmember(
-                device_id = new_stack.id,
-                hardware_id = "DHWAJDJWADDWADWA",
-                member_no = "0",
-                priority = "255",
-            )
-            session.add(stackmember1)
-            self.assertEquals(stackmember1,
-                session.query(Stackmember).filter(Stackmember.device == new_stack).one())
+            session.add(device1)
+            session.add(device2)
+            test_linknet = Linknet(device_a=device1, device_b=device2)
+            device1 = session.query(Device).filter(Device.hostname == 'test-device1').one()
+            device2 = session.query(Device).filter(Device.hostname == 'test-device2').one()
+            self.assertEquals([device2], device1.get_neighbors(session))
+            self.assertEquals([device1], device2.get_neighbors(session))
 
     def test_is_stack(self):
         with sqla_test_session() as session:

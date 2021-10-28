7c82257500f421f7affb71cd3b3e2f84f6424618
Morten Brekkevold <morten.brekkevold@uninett.no>
2021-10-28 16:19:50 +0200

    Make template secrets available as dict in context
    Because:
    - Sometimes, we need to construct secret names from variables that
      are available in the template context. This isn't possible when
      accessing top-level/global context variables, but it is possible
      to construct dictionary keys from variables.
    
    Example:
    
    Looping through a list of tacacs+ servers, each of them have a distinct
    secret key, which we need to fetch from the environment. I.e. the
    environment variables will be different for each server. For the
    tacacs+ server 10.0.1.1 we want to fetch the env var
    TEMPLATE_SECRET_TACPLUS_KEY_10_0_1_1. Jinja doesn't allow us to
    construct this name, but we can get it using something like this:
    {{ TEMPLATE_SECRET['TACPLUS_KEY_' ~ ipaddr] }}

--- src/cnaas_nms/tools/jinja_helpers.py
+++ src/cnaas_nms/tools/jinja_helpers.py
@@ -5,5 +5,9 @@ import os
 def get_environment_secrets(prefix="TEMPLATE_SECRET_"):
     """Returns a dictionary of secrets stored in environment variables"""
     template_secrets = {env: value for env, value in os.environ.items() if env.startswith(prefix)}
+    # Also make secrets available as a dict, so keys can be constructed dynamically in templates
+    template_secrets["TEMPLATE_SECRET"] = {
+        env.replace(prefix, ""): value for env, value in template_secrets.items()
+    }
 
     return template_secrets

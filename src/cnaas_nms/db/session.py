e02af0c88454664e16fe75071ac6d28feb4db755
Simon Oliver Tveit <simtve@uninett.no>
2021-11-18 13:06:08 +0100

    Put sessionmaker code in a function

--- src/cnaas_nms/db/session.py
+++ src/cnaas_nms/db/session.py
@@ -32,15 +32,17 @@ def get_sqlalchemy_conn_str(**kwargs) -> str:
     )
 
 
-conn_str = get_sqlalchemy_conn_str()
-engine = create_engine(conn_str, pool_size=50, max_overflow=50)
-connection = engine.connect()
-Session = sessionmaker(bind=engine)
+def get_session():
+    conn_str = get_sqlalchemy_conn_str()
+    engine = create_engine(conn_str, pool_size=50, max_overflow=50)
+    engine.connect()
+    Session = sessionmaker(bind=engine)
+    return Session()
 
 
 @contextmanager
 def sqla_session(**kwargs):
-    session = Session()
+    session = get_session()
     try:
         yield session
         session.commit()

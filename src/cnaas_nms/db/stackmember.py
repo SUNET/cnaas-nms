0872472a7497a3b5111acdb343f724eb4da8a3b1
Simon Oliver Tveit <simtve@uninett.no>
2021-11-18 15:09:24 +0100

    Add as_dict for stackmember

--- src/cnaas_nms/db/stackmember.py
+++ src/cnaas_nms/db/stackmember.py
@@ -1,9 +1,7 @@
-from sqlalchemy import Column, Integer, UniqueConstraint, String
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, Integer, UniqueConstraint, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
 
 import cnaas_nms.db.base
-
 
 class Stackmember(cnaas_nms.db.base.Base):
     __tablename__ = 'stackmember'
@@ -18,3 +16,12 @@ class Stackmember(cnaas_nms.db.base.Base):
     hardware_id = Column(String(64), nullable=False)
     member_no = Column(Integer)
     priority = Column(Integer)
+
+    def as_dict(self) -> dict:
+        """Return JSON serializable dict."""
+        d = {
+            "member_no": self.member_no,
+            "hardware_id": self.hardware_id,
+            "priority": self.priority,
+        }
+        return d

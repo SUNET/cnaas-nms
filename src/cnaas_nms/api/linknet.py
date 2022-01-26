5d721a759f5e7a6a5169d4a7debbd0cff1bc724b
Johan Marcusson <johan.marcusson@sunet.se>
2022-01-26 14:40:41 +0100

    Allow updating of existing linknets via PUT method. Do data validation with pydantic model for IPs.

--- src/cnaas_nms/api/linknet.py
+++ src/cnaas_nms/api/linknet.py
@@ -1,7 +1,11 @@
+from typing import Optional
+
 from flask import request
 from flask_restx import Resource, Namespace, fields
 from flask_jwt_extended import jwt_required
-from ipaddress import IPv4Network
+from ipaddress import IPv4Network, IPv4Interface, IPv4Address
+from pydantic import BaseModel, validator
+from pydantic.error_wrappers import ValidationError
 
 from cnaas_nms.api.generic import empty_result
 from cnaas_nms.db.session import sqla_session
@@ -9,6 +13,7 @@ from cnaas_nms.db.linknet import Linknet
 from cnaas_nms.db.device import Device, DeviceType
 from cnaas_nms.confpush.underlay import find_free_infra_linknet
 from cnaas_nms.version import __api_version__
+from cnaas_nms.api.generic import parse_pydantic_error, update_sqla_object
 
 
 linknets_api = Namespace('linknets', description='API for handling linknets',
@@ -21,18 +26,79 @@ linknets_model = linknets_api.model('linknets', {
     'device_b': fields.String(required=True),
     'device_a_port': fields.String(required=True),
     'device_b_port': fields.String(required=True),
+    'ipv4_network': fields.String(required=False),
 })
 
 
 linknet_model = linknet_api.model('linknet', {
-    'device_a': fields.String(required=True),
-    'device_b': fields.String(required=True),
-    'device_a_port': fields.String(required=True),
-    'device_b_port': fields.String(required=True),
+    'device_a': fields.String(required=False),
+    'device_b': fields.String(required=False),
+    'device_a_port': fields.String(required=False),
+    'device_b_port': fields.String(required=False),
+    'ipv4_network': fields.String(required=False),
+    'device_a_ip': fields.String(required=False),
+    'device_b_ip': fields.String(required=False),
 })
 
 
+def linknet_device_ip_validator(v, values, **kwargs):
+    if not v:
+        return v
+    if not values['ipv4_network']:
+        raise ValueError("ipv4_network must be set")
+    try:
+        addr = IPv4Address(v)
+        net = IPv4Network(values['ipv4_network'])
+    except:
+        raise ValueError("Invalid device IP or ipv4_network")
+    else:
+        if addr not in net.hosts():
+            raise ValueError("device IP must be part of ipv4_network")
+        if 'device_a_ip' in values and v == values['device_a_ip']:
+            raise ValueError("device_a_ip and device_b_ip can not be the same")
+        if 'device_b_ip' in values and v == values['device_b_ip']:
+            raise ValueError("device_a_ip and device_b_ip can not be the same")
+
+    return v
+
+
+class f_linknet(BaseModel):
+    ipv4_network: Optional[str] = None
+    device_a_ip: Optional[str] = None
+    device_b_ip: Optional[str] = None
+
+    _device_a_ip_valid = validator('device_a_ip', allow_reuse=True)(linknet_device_ip_validator)
+    _device_b_ip_valid = validator('device_b_ip', allow_reuse=True)(linknet_device_ip_validator)
+
+    @validator('ipv4_network')
+    def ipv4_network_validator(cls, v, values, **kwargs):
+        if not v:
+            return v
+        try:
+            net = IPv4Network(v)
+            prefix_len = int(net.prefixlen)
+        except:
+            raise ValueError('Invalid ipv4_network received. Must be IPv4 network address with mask')
+        else:
+            if prefix_len < 29:
+                raise ValueError("Bad prefix length {} for linknet network".format(prefix_len))
+
+        return v
+
+
 class LinknetsApi(Resource):
+    @staticmethod
+    def validate_hostname(hostname):
+        if not Device.valid_hostname(hostname):
+            raise ValueError("Invalid hostname: {}".format(hostname))
+        with sqla_session() as session:
+            dev: Device = session.query(Device). \
+                filter(Device.hostname == hostname).one_or_none()
+            if not dev:
+                raise ValueError("Hostname {} not found in database")
+            # Allow pre-provisioning of linknet to device that is not yet
+            # managed or not assigned device_type, so no further checks here
+
     @jwt_required
     def get(self):
         """ Get all linksnets """
@@ -44,58 +110,53 @@ class LinknetsApi(Resource):
         return empty_result(status='success', data=result)
 
     @jwt_required
-    @linknets_api.expect(linknet_model)
+    @linknets_api.expect(linknets_model)
     def post(self):
         """ Add a new linknet """
         json_data = request.get_json()
         data = {}
         errors = []
-        if 'device_a' in json_data:
-            if not Device.valid_hostname(json_data['device_a']):
-                errors.append("Invalid hostname specified for device_a")
+        for device_arg in ['device_a', 'device_b']:
+            if device_arg in json_data:
+                try:
+                    self.validate_hostname(json_data[device_arg])
+                except ValueError as e:
+                    errors.append("Bad parameter {}: {}".format(device_arg, e))
             else:
-                hostname_a = json_data['device_a']
-        else:
-            errors.append("Required field hostname_a not found")
-        if 'device_b' in json_data:
-            if not Device.valid_hostname(json_data['device_b']):
-                errors.append("Invalid hostname specified for device_b")
-            else:
-                hostname_b = json_data['device_b']
-        else:
-            errors.append("Required field hostname_b not found")
+                errors.append("Required field {} not found".format(device_arg))
+
         if 'device_a_port' not in json_data:
             errors.append("Required field device_a_port not found")
         if 'device_b_port' not in json_data:
             errors.append("Required field device_b_port not found")
 
         new_prefix = None
-        if 'prefix' in json_data:
-            if json_data['prefix']:
+        if 'ipv4_network' in json_data:
+            if json_data['ipv4_network']:
                 try:
-                    new_prefix = IPv4Network(json_data['prefix'])
+                    new_prefix = IPv4Network(json_data['ipv4_network'])
                 except Exception as e:
-                    errors.append("Invalid prefix: {}".format(e))
+                    errors.append("Invalid ipv4_network: {}".format(e))
 
         if errors:
             return empty_result(status='error', data=errors), 400
 
         with sqla_session() as session:
             dev_a: Device = session.query(Device).\
-                filter(Device.hostname == hostname_a).one_or_none()
+                filter(Device.hostname == json_data['device_a']).one_or_none()
             if not dev_a:
                 return empty_result(
                     status='error',
-                    data=f"Hostname '{hostname_a}' not found or is in invalid state"
-                ), 400
+                    data="Hostname '{}' not found".format(json_data['device_a'])
+                ), 500
 
             dev_b: Device = session.query(Device). \
-                filter(Device.hostname == hostname_b).one_or_none()
+                filter(Device.hostname == json_data['device_b']).one_or_none()
             if not dev_b:
                 return empty_result(
                     status='error',
-                    data=f"Hostname '{hostname_b}' not found or is in invalid state"
-                ), 400
+                    data="Hostname '{}' not found".format(json_data['device_b'])
+                ), 500
 
             # check if we need an ip prefix for the linknet
             ip_linknet_devtypes = [DeviceType.CORE, DeviceType.DIST]
@@ -111,8 +172,8 @@ class LinknetsApi(Resource):
 
             try:
                 new_linknet = Linknet.create_linknet(
-                    session, hostname_a, json_data['device_a_port'],
-                    hostname_b, json_data['device_b_port'], new_prefix)
+                    session, json_data['device_a'], json_data['device_a_port'],
+                    json_data['device_b'], json_data['device_b_port'], new_prefix)
                 session.add(new_linknet)
                 session.commit()
                 data = new_linknet.as_dict()
@@ -148,7 +209,7 @@ class LinknetsApi(Resource):
 class LinknetByIdApi(Resource):
     @jwt_required
     def get(self, linknet_id):
-        """ Get all linksnets """
+        """ Get a single specified linknet """
         result = empty_result()
         result['data'] = {'linknets': []}
         with sqla_session() as session:
@@ -174,6 +235,45 @@ class LinknetByIdApi(Resource):
                                     data={"deleted_linknet": instance.as_dict()}), 200
             else:
                 return empty_result('error', "No such linknet found in database"), 404
+
+    @jwt_required
+    @linknets_api.expect(linknet_model)
+    def put(self, linknet_id):
+        """ Update data on existing linknet """
+        json_data = request.get_json()
+        data = {}
+        errors = []
+        for device_arg in ['device_a', 'device_b']:
+            if device_arg in json_data:
+                try:
+                    LinknetsApi.validate_hostname(json_data[device_arg])
+                except ValueError as e:
+                    errors.append("Bad parameter {}: {}".format(device_arg, e))
+
+        if errors:
+            return empty_result(status='error', data=errors), 400
+
+        with sqla_session() as session:
+            instance: Linknet = session.query(Linknet). \
+                filter(Linknet.id == linknet_id).one_or_none()
+            if instance:
+                try:
+                    validate_data = {**instance.as_dict(), **json_data}
+                    f_linknet(**validate_data).dict()
+                except ValidationError as e:
+                    errors += parse_pydantic_error(e, f_linknet, validate_data)
+                if errors:
+                    return empty_result(status='error', data=errors), 400
+
+                changed: bool = update_sqla_object(instance, json_data)
+                if changed:
+                    instance.device_a.synchronized = False
+                    instance.device_b.synchronized = False
+                    return empty_result(status='success', data={"updated_linknet": instance.as_dict()}), 200
+                else:
+                    return empty_result(status='success', data={"unchanged_linknet": instance.as_dict()}), 200
+            else:
+                return empty_result(status='error', data="linknet not found"), 400
 
 
 # Links

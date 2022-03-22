1399a434bbc1b65cd8f4968b2570bf9587d59743
Johan Marcusson <johan.marcusson@sunet.se>
2022-03-22 09:51:16 +0100

    Create setup_package for standalone unittests

--- src/cnaas_nms/__init__.py
+++ src/cnaas_nms/__init__.py
@@ -1,1 +1,9 @@
 __import__('pkg_resources').declare_namespace(__name__)
+
+
+def setup_package():
+    import cnaas_nms.api.app
+    client = cnaas_nms.api.app.app.test_client()
+    data = {"action": "refresh"}
+    client.put('/api/v1.0/repository/settings', json=data)
+    client.put('/api/v1.0/repository/templates', json=data)

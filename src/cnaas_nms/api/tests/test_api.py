ca819cca4c58d5b2d3c7e70b469c6755dd81e041
Johan Marcusson <johan.marcusson@sunet.se>
2022-03-22 09:51:53 +0100

    Since /opt/cnaas/settings is a volume we should probably remove contents of dir instead of entire dir

--- src/cnaas_nms/api/tests/test_api.py
+++ src/cnaas_nms/api/tests/test_api.py
@@ -25,7 +25,7 @@ class ApiTests(unittest.TestCase):
         hostname = self.testdata['managed_dist']
         result = self.client.get(
             f'/api/v1.0/devices',
-            params={"filter[hostname]": hostname}
+            query_string={"filter[hostname]": hostname}
         )
 
         # 200 OK
@@ -120,8 +120,13 @@ class ApiTests(unittest.TestCase):
         self.assertRegex(result.json['data'], r'[Cc]ommit')
 
     def test_repository_get(self):
-        # Delete settings repo
-        shutil.rmtree(app_settings.SETTINGS_LOCAL)
+        # Delete everything inside setting repo
+        for filename in os.listdir(app_settings.SETTINGS_LOCAL):
+            file_path = os.path.join(app_settings.SETTINGS_LOCAL, filename)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
 
         # Check that no repo is found
         result = self.client.get('/api/v1.0/repository/settings')

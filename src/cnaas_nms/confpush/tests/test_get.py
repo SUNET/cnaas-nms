6c79aeb6278e0e4f3589ae793cc3ec58468fd265
Johan Marcusson <johan.marcusson@sunet.se>
2022-11-10 10:10:07 +0100

    unittest for get_mlag_ifs

--- src/cnaas_nms/confpush/tests/test_get.py
+++ src/cnaas_nms/confpush/tests/test_get.py
@@ -1,14 +1,13 @@
-import cnaas_nms.confpush.get
-
 import pprint
 import unittest
 import pkg_resources
 import yaml
 import os
 
+import cnaas_nms.confpush.get
 import cnaas_nms.confpush.update
 from cnaas_nms.db.session import sqla_session
-from cnaas_nms.db.device import DeviceType
+from cnaas_nms.db.device import Device, DeviceType, DeviceState
 
 
 class GetTests(unittest.TestCase):
@@ -16,6 +15,17 @@ class GetTests(unittest.TestCase):
         data_dir = pkg_resources.resource_filename(__name__, 'data')
         with open(os.path.join(data_dir, 'testdata.yml'), 'r') as f_testdata:
             self.testdata = yaml.safe_load(f_testdata)
+
+    @classmethod
+    def create_test_device(cls, hostname="unittest"):
+        return Device(
+            ztp_mac="000000000000",
+            hostname=hostname,
+            platform="eos",
+            management_ip=None,
+            state=DeviceState.MANAGED,
+            device_type=DeviceType.ACCESS,
+        )
 
     def test_get_inventory(self):
         result = cnaas_nms.confpush.get.get_inventory()
@@ -28,6 +38,45 @@ class GetTests(unittest.TestCase):
         self.assertLessEqual(
             1,
             len(result['hosts'].items()))
+
+    def test_get_mlag_ifs(self):
+        with sqla_session() as session:
+            try:
+                dev_a: Device = self.create_test_device(self.testdata['mlag_dev_a'])
+                dev_b: Device = self.create_test_device(self.testdata['mlag_dev_b'])
+                dev_nonpeer: Device = self.create_test_device(self.testdata['mlag_dev_nonpeer'])
+                session.add(dev_a)
+                session.add(dev_b)
+                session.add(dev_nonpeer)
+                session.commit()
+                linknets = []
+                for linknet in self.testdata['linknets_mlag_peers']:
+                    linknet['device_a_hostname'] = dev_a.hostname
+                    linknet['device_a_id'] = dev_a.id
+                    linknet['device_b_hostname'] = dev_b.hostname
+                    linknet['device_b_id'] = dev_b.id
+                    linknets.append(linknet)
+                for linknet in self.testdata['linknets_mlag_nonpeers']:
+                    linknet['device_a_hostname'] = dev_a.hostname
+                    linknet['device_a_id'] = dev_a.id
+                    linknet['device_b_hostname'] = dev_nonpeer.hostname
+                    linknet['device_b_id'] = dev_nonpeer.id
+                    linknets.append(linknet)
+
+                res = cnaas_nms.confpush.get.get_mlag_ifs(
+                    session, dev_a, self.testdata['mlag_dev_b'], linknets)
+                self.assertEqual(res, {'Ethernet25': dev_b.id, 'Ethernet26': dev_b.id})
+            except Exception as e:
+                session.rollback()
+                session.delete(dev_a)
+                session.delete(dev_b)
+                session.delete(dev_nonpeer)
+                raise e
+            else:
+                session.rollback()
+                session.delete(dev_a)
+                session.delete(dev_b)
+                session.delete(dev_nonpeer)
 
     def equipmenttest_update_links(self):
         with sqla_session() as session:

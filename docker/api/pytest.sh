a02c08472938e09a835dc3a7ddd64a36d8bf8e76
Johan Marcusson <johan.marcusson@sunet.se>
2022-12-21 16:57:46 +0100

    Make pytest fixtures not start dockers when running integrationtests already in docker

--- docker/api/pytest.sh
+++ docker/api/pytest.sh
@@ -4,5 +4,5 @@ cd /opt/cnaas/venv/cnaas-nms/src/
 
 source ../../bin/activate
 
-pytest --cov=cnaas_nms
+pytest --cov=cnaas_nms -p no:cacheprovider
 mv .coverage .coverage-pytest
--- src/conftest.py
+++ src/conftest.py
@@ -1,5 +1,6 @@
 import time
 import socket
+import os
 from contextlib import closing
 
 from git import Repo
@@ -20,38 +21,49 @@ def pytest_configure(config):
 def settings_directory(tmp_path_factory):
     from cnaas_nms.app_settings import app_settings
 
-    settings_dir = tmp_path_factory.mktemp("settings")
-    app_settings.SETTINGS_LOCAL = settings_dir
-    print(f"placing settings in {settings_dir}")
-    Repo.clone_from(app_settings.SETTINGS_REMOTE, app_settings.SETTINGS_LOCAL)
-    return settings_dir
+    if os.getenv('PYTEST_SETTINGS_CLONED', '0').strip() in ('0', 'off', 'false', 'no'):
+        settings_dir = tmp_path_factory.mktemp("settings")
+        app_settings.SETTINGS_LOCAL = settings_dir
+        print(f"placing settings in {settings_dir}")
+        Repo.clone_from(app_settings.SETTINGS_REMOTE, app_settings.SETTINGS_LOCAL)
+        return settings_dir
+    else:
+        return app_settings.SETTINGS_LOCAL
 
 
 @pytest.fixture(scope="session")
 def templates_directory(tmp_path_factory):
     from cnaas_nms.app_settings import app_settings
 
-    templates_dir = tmp_path_factory.mktemp("templates")
-    app_settings.TEMPLATES_LOCAL = templates_dir
-    print(f"placing settings in {templates_dir}")
-    Repo.clone_from(app_settings.TEMPLATES_REMOTE, app_settings.TEMPLATES_LOCAL)
-    return templates_dir
+    if os.getenv('PYTEST_TEMPLATES_CLONED', '0').strip() in ('0', 'off', 'false', 'no'):
+        templates_dir = tmp_path_factory.mktemp("templates")
+        app_settings.TEMPLATES_LOCAL = templates_dir
+        print(f"placing settings in {templates_dir}")
+        Repo.clone_from(app_settings.TEMPLATES_REMOTE, app_settings.TEMPLATES_LOCAL)
+        return templates_dir
+    else:
+        return app_settings.TEMPLATES_LOCAL
 
 
 @pytest.fixture(scope="session")
-def redis(session_scoped_container_getter):
+def redis(request):
     """Ensures Redis server is running and available"""
     # This uses pytest-docker-compose, but could also just use pytest-docker for fewer moving parts
-    assert wait_for_port('127.0.0.1', 6379), "Could not connect to Redis"
+    if os.getenv('PYTEST_REDIS_EXTERNAL', '0').strip() in ('0', 'off', 'false', 'no'):
+        request.getfixturevalue("session_scoped_container_getter")
+        assert wait_for_port('127.0.0.1', 6379), "Could not connect to Redis"
     yield True
 
+
 @pytest.fixture(scope="session")
-def postgresql(session_scoped_container_getter):
+def postgresql(request):
     """Ensures PostgreSQL server is running and available"""
     # This uses pytest-docker-compose, but could also just use pytest-docker for fewer moving parts
     # It could also just use pytest-postgresql (which has options to load specific SQL fixtures as
     # well)
-    assert wait_for_port('127.0.0.1', 5432), "Could not connect to PostgreSQL"
+    if os.getenv('PYTEST_POSTGRES_EXTERNAL', '0').strip() in ('0', 'off', 'false', 'no'):
+        request.getfixturevalue("session_scoped_container_getter")
+        assert wait_for_port('127.0.0.1', 5432), "Could not connect to PostgreSQL"
     # There is an apparent lag between the server responding to TCP port 5432 and actually being
     # ready to serve database connections. Sleeping for 1 second is the naive solution here,
     # but it's okay, since it only happens the first time the fixture is used in a test session.

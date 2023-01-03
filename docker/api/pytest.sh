366ecaa8730ee2c5d4e870bccee33e52d6dd43c2
Johan Marcusson <johan.marcusson@sunet.se>
2023-01-03 16:57:44 +0100

    pass not equipment through to pytest

--- docker/api/pytest.sh
+++ docker/api/pytest.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+PYTESTARGS=""
+
+if [ ! -z "$NO_EQUIPMENTTEST" ] ; then
+	PYTESTARGS="-m 'not equipment'"
+fi
+
 cd /opt/cnaas/venv/cnaas-nms/src/
 
 source ../../bin/activate
 
-pytest --cov=cnaas_nms -p no:cacheprovider
+pytest --cov=cnaas_nms -p no:cacheprovider $PYTESTARGS
 EXITSTATUS="$?"
 mv .coverage .coverage-pytest
 

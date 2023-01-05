ce48c32211ffdf14c8ec36f115d185d8c99a18ef
Johan Marcusson <johan.marcusson@sunet.se>
2023-01-05 10:42:24 +0100

    "Beyond making pep8 happy, I think this is a better solution because NULL is not a valid as the RHS of != in SQL and using isnot better conveys your intentions for what you want the generated statement to look like." https://stackoverflow.com/questions/21784851/sqlalchemy-is-not-null-select

--- src/cnaas_nms/devicehandler/underlay.py
+++ src/cnaas_nms/devicehandler/underlay.py
@@ -12,7 +12,7 @@ from cnaas_nms.db.settings import get_settings
 def find_free_infra_ip(session) -> Optional[IPv4Address]:
     """Returns first free IPv4 infra IP."""
     used_ips = []
-    device_query = session.query(Device).filter(Device.infra_ip != None).options(load_only("infra_ip"))  # noqa: E711
+    device_query = session.query(Device).filter(Device.infra_ip.isnot(None)).options(load_only("infra_ip"))
     for device in device_query:
         used_ips.append(device.infra_ip)
     settings, settings_origin = get_settings(device_type=DeviceType.CORE)
@@ -30,9 +30,7 @@ def find_free_mgmt_lo_ip(session) -> Optional[IPv4Address]:
     """Returns first free IPv4 infra IP."""
     used_ips = []
     reserved_ips = []
-    device_query = (
-        session.query(Device).filter(Device.management_ip != None).options(load_only("management_ip"))  # noqa: E711
-    )
+    device_query = session.query(Device).filter(Device.management_ip.isnot(None)).options(load_only("management_ip"))
     for device in device_query:
         used_ips.append(device.management_ip)
     reserved_ip_query = session.query(ReservedIP).options(load_only("ip"))
@@ -55,7 +53,7 @@ def find_free_mgmt_lo_ip(session) -> Optional[IPv4Address]:
 def find_free_infra_linknet(session) -> Optional[IPv4Network]:
     """Returns first free IPv4 infra linknet (/31)."""
     used_linknets = []
-    linknet_query = session.query(Linknet).filter(Linknet.device_a_ip != None)  # noqa: E711
+    linknet_query = session.query(Linknet).filter(Linknet.device_a_ip.isnot(None))
     ln: Linknet
     for ln in linknet_query:
         used_linknets.append(IPv4Interface(ln.ipv4_network).network)

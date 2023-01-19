3a9dec25d126f5be76bf05042845ed40f609d3a2
Johan Marcusson <johan.marcusson@sunet.se>
2023-01-19 16:37:50 +0100

    try to get code coverage for the confirm_devices function

--- src/cnaas_nms/devicehandler/tests/test_syncto.py
+++ src/cnaas_nms/devicehandler/tests/test_syncto.py
@@ -30,13 +30,14 @@ def scheduler(scope="session"):
     return scheduler
 
 
-def run_syncto_job(scheduler, testdata: dict) -> Optional[dict]:
+def run_syncto_job(scheduler, testdata: dict, dry_run: bool = True) -> Optional[dict]:
     job_id = scheduler.add_onetime_job(
         sync_devices,
         when=0,
         scheduled_by="test_user",
         kwargs={
             "hostnames": testdata["syncto_device_hostnames"],
+            "dry_run": dry_run,
         },
     )
     job_res: Optional[Job] = None
@@ -61,7 +62,7 @@ def test_syncto_commitmode_0(testdata, scheduler, settings_directory, templates_
         hostname = testdata["syncto_device_hostnames"][0]
         assert f"Commit confirm mode for host {hostname}: 0" in caplog.text
     assert job_dict["status"] == "FINISHED"
-    assert job_dict["result"]["devices"]["eosdist1"]["failed"] is False
+    assert job_dict["result"]["devices"][hostname]["failed"] is False
 
 
 @pytest.mark.equipment
@@ -73,7 +74,7 @@ def test_syncto_commitmode_1(testdata, scheduler, settings_directory, templates_
         hostname = testdata["syncto_device_hostnames"][0]
         assert f"Commit confirm mode for host {hostname}: 1" in caplog.text
     assert job_dict["status"] == "FINISHED"
-    assert job_dict["result"]["devices"]["eosdist1"]["failed"] is False
+    assert job_dict["result"]["devices"][hostname]["failed"] is False
 
 
 @pytest.mark.equipment
@@ -81,8 +82,16 @@ def test_syncto_commitmode_2(testdata, scheduler, settings_directory, templates_
     api_settings.COMMIT_CONFIRMED_MODE = 2
     api_settings.SETTINGS_OVERRIDE = testdata["syncto_settings_override"]
     with caplog.at_level(logging.DEBUG):
-        job_dict = run_syncto_job(scheduler, testdata)
+        job_dict = run_syncto_job(scheduler, testdata, dry_run=False)
         hostname = testdata["syncto_device_hostnames"][0]
         assert f"Commit confirm mode for host {hostname}: 2" in caplog.text
     assert job_dict["status"] == "FINISHED"
-    assert job_dict["result"]["devices"]["eosdist1"]["failed"] is False
+    assert job_dict["result"]["devices"][hostname]["failed"] is False
+
+    # Revert change
+    api_settings.SETTINGS_OVERRIDE = None
+    with caplog.at_level(logging.DEBUG):
+        job_dict = run_syncto_job(scheduler, testdata, dry_run=False)
+        assert "selected for commit-confirm" in caplog.text
+    assert job_dict["status"] == "FINISHED"
+    assert job_dict["result"]["devices"][hostname]["failed"] is False

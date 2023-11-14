424a25be1012e397a8ecc9fe8e20e3e7dab8d809
josephine-jc <lab@example.com>
2023-11-14 11:58:05 +0100

    Added roles etc to security

--- src/cnaas_nms/tools/security.py
+++ src/cnaas_nms/tools/security.py
@@ -2,16 +2,20 @@ from flask_jwt_extended import get_jwt_identity as get_jwt_identity_orig
 from flask_jwt_extended import jwt_required as jwt_orig
 from authlib.integrations.flask_oauth2 import ResourceProtector, current_token
 from authlib.oauth2.rfc6750 import errors, BearerTokenValidator
+from authlib.oauth2.rfc6749.wrappers import HttpRequest
 from jose import jwt
 from jose import exceptions
-from jwt.exceptions import InvalidTokenError, InvalidKeyError, ExpiredSignatureError
+from jwt.exceptions import InvalidTokenError, InvalidKeyError, ExpiredSignatureError, InvalidAudienceError
 from flask_jwt_extended.exceptions import NoAuthorizationError
 from authlib.oauth2.rfc6749 import MissingAuthorizationError
 import requests
-from typing import Mapping, Any
+from typing import Mapping, Any, List
+import re
 
 from cnaas_nms.tools.log import get_logger
 from cnaas_nms.app_settings import auth_settings, api_settings
+from cnaas_nms.version import __api_version__
+import json
 
 logger = get_logger()
 
@@ -38,6 +42,22 @@ class MyResourceProtector(ResourceProtector):
             raise NoAuthorizationError(error) 
         raise error
 
+
+class Token():
+    token_string: str = ""
+    decoded_token = {}
+    token_type: str = ""
+    audience: str = ""
+    expires_at = ""
+
+    def __init__(self, token_string, decoded_token, algorithm):
+        self.token_string = token_string
+        self.decoded_token = decoded_token
+        self.token_type = algorithm
+        self.audience = auth_settings.OIDC_CLIENT_ID
+        self.expires_at = decoded_token["exp"]
+    def get_scope():
+        return
 
 class MyBearerTokenValidator(BearerTokenValidator):
     keys: Mapping[str, Any] = {}
@@ -113,13 +133,88 @@ class MyBearerTokenValidator(BearerTokenValidator):
             "expires_at": decoded_token["exp"]
         }
         return token
+    
 
-    def validate_token(self, token, scopes, request):
-        """Check if token matches the requested scopes."""
-        # For now we don't have a scope yet
-        # When needed, look at implementation example here:
-        # https://github.com/lepture/authlib/blob/master/authlib/oauth2/rfc6750/validator.py
-        return token
+    def load_role_yaml():
+        '''Load the file with role permission'''
+        # TODO check if we want to load this in once and put it in a different place
+        # TODO already give back if stuff is missing fromthis file/formatting is wrong etc
+        import yaml
+        try:
+            with open("roles.yml", "r") as roles_file:
+                roles_data = yaml.safe_load(roles_file)
+        except FileNotFoundError:
+            logger.debug('No Roles file. All roles get * rights. ')
+            return None
+        return roles_data
+    
+
+    def get_user_role_from_dic(roles_data, decoded_token):
+        '''Get the API permissions of the user'''
+        if 'role_in_jwt_element' not in roles_data['config']:
+            if "fallback_role" in roles_data["config"] and  roles_data["config"]["fallback_role"] in roles_data["roles"]:
+                    user_role = roles_data["config"]["fallback_role"]
+            else:
+                #TODO maybe just make the scope the fallback?
+                logger.debug('Bad Roles file. All roles get * rights. ')
+                return None
+        if roles_data['config']['role_in_jwt_element'] not in decoded_token:
+            if "fallback_role" in roles_data["config"] and  roles_data["config"]["fallback_role"] in roles_data["roles"]:
+                user_role = roles_data["config"]["fallback_role"]
+            else:
+                logger.debug("User doesn't have element :'" + roles_data['config']['role_in_jwt_element'] + "' in JWT token.")
+                raise InvalidAudienceError()
+        
+        # select the right role
+        if 'roles_seperated_by' not in roles_data['config'] or roles_data['config']['roles_seperated_by'] == "":
+            user_role = decoded_token[roles_data['config']['role_in_jwt_element']]
+            user_roles = [user_role]
+        else:
+            user_roles = decoded_token[roles_data['config']['role_in_jwt_element']].split(roles_data['config']['roles_seperated_by'])
+            user_role = None
+            for role in user_roles:
+                if role in roles_data["roles"]:
+                    user_role = role
+        if user_role not in roles_data["roles"]:
+            if "any" in roles_data["roles"]:
+                    user_role = "any"
+            else:
+                logger.debug('Requested roles: [' + ','.join(user_roles) + ']. Roles not found in roles.yaml. ')
+                raise InvalidAudienceError()
+        
+        # get the permissions of the role
+        allowed_api_methods: List[str] = roles_data["roles"][user_role]['allowed_api_methods']
+        allowed_api_calls: List[str] = roles_data["roles"][user_role]['allowed_api_calls']
+
+        return allowed_api_methods, allowed_api_calls
+    
+
+    def check_if_allowed_to_make_api_call(request: HttpRequest, allowed_api_methods: List[str], allowed_api_calls: List[str]):
+        '''Checks if the user has permission to execute the API call'''
+        if "*" not in allowed_api_methods and request.method not in allowed_api_methods:
+            raise InvalidAudienceError()
+        
+        prefix = "/api/{}".format(__api_version__)
+        short_uri = request.uri[:-1].strip().removeprefix(prefix)
+        # added the regex so it's easier to add a bunch of api calls (like all device api calls)
+        combined = "(" + ")|(".join(allowed_api_calls) + ")"
+         # check if you're permitted to make api call based on uri
+        if "*" not in allowed_api_calls and short_uri not in allowed_api_calls and not re.fullmatch(combined, short_uri):
+            raise InvalidAudienceError()
+        # return the token
+        return True 
+    
+
+    def validate_token(self, token, scopes, request: HttpRequest):
+        """Check if token matches the requested scopes and user has permission to execute the API call."""
+        roles_data = self.load_role_yaml()
+        if roles_data is None:
+            logger.debug('No Roles file. All roles get * rights. ')
+            return token
+        allowed_api_methods, allowed_api_calls = self.get_user_role_from_dic(roles_data, token["decoded_token"])
+        if(self.check_if_allowed_to_make_api_call(request, allowed_api_methods, allowed_api_calls)):
+            return token
+
 
 
 def get_oauth_identity():

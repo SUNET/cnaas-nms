b0cc14a37f34c494da665aa15af1dce30f7076d2
josephine-jc <josephine.rutten@surf.nl>
2023-12-12 11:35:50 +0100

    worked on unit tests and improved the short uri

--- src/cnaas_nms/tools/rbac/rbac.py
+++ src/cnaas_nms/tools/rbac/rbac.py
@@ -46,9 +46,7 @@ def check_if_api_call_is_permitted(request: HttpRequest, permissions_of_user):
 
         # prepare the uri
         prefix = "/api/{}".format(__api_version__)
-        # TODO better solution for removing one char
-        short_uri = request.uri[:-1].strip().removeprefix(prefix)
-        short_uri = short_uri.split('?', 1)[0]
+        short_uri = request.uri.strip().removeprefix(prefix).split('?', 1)[0]
 
         # check if you're permitted to make api call based on uri
         if "*" in allowed_endpoints or short_uri in allowed_endpoints:
--- src/cnaas_nms/tools/rbac/tests/test_security_rbac.py
+++ src/cnaas_nms/tools/rbac/tests/test_security_rbac.py
@@ -1,5 +1,7 @@
 import unittest
-
+import pathlib
+import fnmatch
+from pathlib import Path
 from cnaas_nms.tools.rbac.rbac import check_if_api_call_is_permitted, get_permissions_user
 from authlib.oauth2.rfc6749.wrappers import HttpRequest
 from cnaas_nms.version import __api_version__
@@ -7,130 +9,115 @@ from cnaas_nms.version import __api_version__
 class CheckRoleRBACTests(unittest.TestCase):
     prefix = "/api/{}".format(__api_version__)
     def test_user_is_allowed_api_call(self):
-        request = HttpRequest("GET", self.prefix + "/uri/")
-        permissions_of_user = {"role":{"allowed_api_methods": ["GET"], "allowed_api_calls" : ["/uri"]}}
+        request = HttpRequest("GET", self.prefix + "/devices")
+        permissions_of_user = [{'methods': ['GET' ,'POST'], 'endpoints': ['/auth/*', '/devices']}]
         is_allowed = check_if_api_call_is_permitted(request, permissions_of_user)
         self.assertEqual(is_allowed, True)
     
     def test_user_is_allowed_api_call_with_star(self):
-        request = HttpRequest("GET", self.prefix  + "/uri/")
-        permissions_of_user = {"role":{"allowed_api_methods": ["*"], "allowed_api_calls" : ["*"]}}
+        request = HttpRequest("GET", self.prefix  + "/uri")
+        permissions_of_user = [{"methods": ["*"], "endpoints" : ["*"]}]
         is_allowed = check_if_api_call_is_permitted(request, permissions_of_user)
         self.assertEqual(is_allowed, True)
     
-    def test_user_is_allowed_api_call_with_regex(self):
+    def test_user_is_allowed_api_call_with_glob(self):
         request = HttpRequest("GET", self.prefix + "/uri/test")
-        permissions_of_user = {"role":{"allowed_api_methods": ["GET"], "allowed_api_calls" : ["/uri/.*"]}}
+        permissions_of_user = [{"methods": ["GET"], "endpoints" : ["/uri/*"]}]
+        is_allowed = check_if_api_call_is_permitted(request, permissions_of_user)
+        self.assertEqual(is_allowed, True)
+    
+    def test_user_is_allowed_api_call_with_glob_double_star(self):
+        request = HttpRequest("GET", self.prefix + "/uri/test/test")
+        permissions_of_user = [{"methods": ["GET"], "endpoints" : ["/uri/**"]}]
         is_allowed = check_if_api_call_is_permitted(request, permissions_of_user)
         self.assertEqual(is_allowed, True)
 
-    def test_user_is_allowed_api_call_in_2nd_role(self):
+    def test_user_is_allowed_api_call_multiple_permissions(self):
         request = HttpRequest("GET", self.prefix + "/uri")
-        permissions_of_user = {"role":{"allowed_api_methods": ["GET"], "allowed_api_calls" : ["no"]}, "role2":{"allowed_api_methods": ["GET"], "allowed_api_calls" : ["*"]}}
+        permissions_of_user = [{"methods": ["GET"], "endpoints" : ["no"]}, {"methods": ["GET"], "endpoints" : ["*"]}]
         is_allowed = check_if_api_call_is_permitted(request, permissions_of_user)
         self.assertEqual(is_allowed, True)
     
-    def test_user_is_not_allowed_api_call(self):
+    def test_user_is_not_allowed_api_call_method(self):
         request = HttpRequest("POST", self.prefix + "/uri")
-        permissions_of_user = {"role":{"allowed_api_methods": ["GET"], "allowed_api_calls" : ["*"]}}
+        permissions_of_user = [{"methods": ["GET"], "endpoints" : ["*"]}]
         is_allowed = check_if_api_call_is_permitted(request, permissions_of_user)
         self.assertEqual(is_allowed, False)
 
-    def test_user_is_not_allowed_api_call_with_regex(self):
+    def test_user_is_not_allowed_api_call_with_glob(self):
         request = HttpRequest("GET", self.prefix  + "/different/test")
-        permissions_of_user = {"role":{"allowed_api_methods": ["GET"], "allowed_api_calls" : ["/uri/.*"]}}
+        permissions_of_user =[{"methods": ["GET"], "endpoints" : ["/uri/**"]}]
         is_allowed = check_if_api_call_is_permitted(request, permissions_of_user)
         self.assertEqual(is_allowed, False)
 
     def test_user_is_not_allowed_api_call_empty(self):
         request = HttpRequest("GET", self.prefix + "/different/test")
-        permissions_of_user = {}
+        permissions_of_user = []
         is_allowed = check_if_api_call_is_permitted(request, permissions_of_user)
         self.assertEqual(is_allowed, False)
     
     def test_user_is_not_allowed_api_call_in_2_roles(self):
-        request = HttpRequest("POST", self.prefix + "/different/test")
-        permissions_of_user = {"role":{"allowed_api_methods": ["*"], "allowed_api_calls" : ["no"]}, "role2":{"allowed_api_methods": ["GET"], "allowed_api_calls" : ["*"]}}
+        request = HttpRequest("POST", self.prefix + "/different/test/test")
+        permissions_of_user = [{"methods": ["*"], "endpoints" : ["/uri/*"]}, {"methods": ["GET"], "endpoints" : ["*"]}]
         is_allowed = check_if_api_call_is_permitted(request, permissions_of_user)
         self.assertEqual(is_allowed, False)
 
 class GetPremissionsRoleYamlTests(unittest.TestCase):
 
     def test_role_permissions_with_default(self):
-        permissions_rules = {'config': {'role_in_jwt_element': 'edumember_is_member_of', 'roles_separated_by': ':', 'default_permissions': 'any'}, 'roles': {'read': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs', '*']}, 'write': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['*']}, 'admin': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['/devices', '/jobs', '/repository/y.*']}, 'any': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs']}}}
-        user_info = {"edumember_is_member_of":"read:write"}
+        permissions_rules = {'config': {'group_claim_key': 'edumember_is_member_of', 'default_permissions': 'default'}, 'roles': {'urn:collab:group:test.surfconext.nl:nl:surfnet:diensten:surfwired-admin': {'permissions': [{'methods': ['GET', 'PUT'], 'endpoints': ['/devices/**/interfaces', '/repository']}, {'methods': ['POST'], 'endpoints': ['/auth/*', '/devices']}]}, 'default': {'permissions': [{'methods': ['GET'], 'endpoints': ['/devices**']}]}}}
+        user_info = {"edumember_is_member_of":"urn:collab:group:test.surfconext.nl:nl:surfnet:diensten:surfwired-admin"}
         permissions_of_user = get_permissions_user(permissions_rules, user_info)
-        expected_result = {'write': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['*']}, 'read': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs', '*']}, 'any': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs']}}
+        expected_result =[{'methods': ['GET'], 'endpoints': ['/devices**']}, {'methods': ['GET', 'PUT'], 'endpoints': ['/devices/**/interfaces', '/repository']}, {'methods': ['POST'], 'endpoints': ['/auth/*', '/devices']}]
         self.assertEqual(permissions_of_user, expected_result)
 
     def test_role_permissions(self):
-        permissions_rules = {'config': {'role_in_jwt_element': 'edumember_is_member_of', 'roles_separated_by': ':'}, 'roles': {'read': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs', '*']}, 'write': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['*']}, 'admin': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['/devices', '/jobs', '/repository/y.*']}, 'any': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs']}}}
-        user_info = {"edumember_is_member_of":"read:write"}
+        permissions_rules = {'config': {'group_claim_key': 'edumember_is_member_of', 'default_permissions': 'default'}, 'roles': {'urn:collab:group:test.surfconext.nl:nl:surfnet:diensten:surfwired-admin': {'permissions': [{'methods': ['GET', 'PUT'], 'endpoints': ['/devices/**/interfaces', '/repository']}, {'methods': ['POST'], 'endpoints': ['/auth/*', '/devices']}]}}}
+        user_info = {"edumember_is_member_of":"urn:collab:group:test.surfconext.nl:nl:surfnet:diensten:surfwired-admin"}
         permissions_of_user = get_permissions_user(permissions_rules, user_info)
-        expected_result = {'write': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['*']}, 'read': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs', '*']}}
+        expected_result = [{'methods': ['GET', 'PUT'], 'endpoints': ['/devices/**/interfaces', '/repository']}, {'methods': ['POST'], 'endpoints': ['/auth/*', '/devices']}]
         self.assertEqual(permissions_of_user, expected_result)
 
     def test_role_permissions_only_default(self):
-        permissions_rules = {'config': {'role_in_jwt_element': 'edumember_is_member_of', 'roles_separated_by': ':', 'default_permissions': 'any'}, 'roles': {'read': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs', '*']}, 'write': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['*']}, 'admin': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['/devices', '/jobs', '/repository/y.*']}, 'any': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs']}}}
-        user_info = {"edumember_is_member_of":"test:test2"}
+        permissions_rules = {'config': {'group_claim_key': 'edumember_is_member_of', 'default_permissions': 'default'}, 'roles': {'urn:collab:group:test.surfconext.nl:nl:surfnet:diensten:surfwired-admin': {'permissions': [{'methods': ['GET', 'PUT'], 'endpoints': ['/devices/**/interfaces', '/repository']}, {'methods': ['POST'], 'endpoints': ['/auth/*', '/devices']}]}, 'default': {'permissions': [{'methods': ['GET'], 'endpoints': ['/devices**']}]}}}
+        user_info = {"edumember_is_member_of":"notarealrole"}
         permissions_of_user = get_permissions_user(permissions_rules, user_info)
-        expected_result = {'any': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs']}}
+        expected_result = [{'methods': ['GET'], 'endpoints': ['/devices**']}]
         self.assertEqual(permissions_of_user, expected_result)
 
     def test_role_permissions_zero(self):
-        permissions_rules = {'config': {'role_in_jwt_element': 'edumember_is_member_of', 'roles_separated_by': ':'}, 'roles': {'read': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs', '*']}, 'write': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['*']}, 'admin': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['/devices', '/jobs', '/repository/y.*']}, 'any': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs']}}}
-        user_info = {"edumember_is_member_of":"test"}
+        permissions_rules = {'config': {'group_claim_key': 'edumember_is_member_of', 'default_permissions': 'default'}, 'roles': {'urn:collab:group:test.surfconext.nl:nl:surfnet:diensten:surfwired-admin': {'permissions': [{'methods': ['GET', 'PUT'], 'endpoints': ['/devices/**/interfaces', '/repository']}, {'methods': ['POST'], 'endpoints': ['/auth/*', '/devices']}]}}}
+        user_info = {"edumember_is_member_of":"notarealrole"}
         permissions_of_user = get_permissions_user(permissions_rules, user_info)
-        expected_result = {}
-        self.assertEqual(permissions_of_user, expected_result)
-
-    def test_role_permissions_default_expected_not_defined(self):
-        permissions_rules = {'config': {'role_in_jwt_element': 'edumember_is_member_of', 'roles_separated_by': ':', 'default_permissions': 'any'}, 'roles': {'read': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs', '*']}, 'write': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['*']}, 'admin': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['/devices', '/jobs', '/repository/y.*']}}}
-        user_info = {"edumember_is_member_of":"test"}
-        permissions_of_user = get_permissions_user(permissions_rules, user_info)
-        expected_result = {}
+        expected_result = []
         self.assertEqual(permissions_of_user, expected_result)
 
     def test_user_no_element_default(self):
-        permissions_rules = {'config': {'role_in_jwt_element': 'edumember_is_member_of', 'roles_separated_by': ':', 'default_permissions': 'any'}, 'roles': {'read': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs', '*']}, 'write': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['*']}, 'admin': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['/devices', '/jobs', '/repository/y.*']}, 'any': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs']}}}
+        permissions_rules = {'config': {'group_claim_key': 'edumember_is_member_of', 'default_permissions': 'default'}, 'roles': {'urn:collab:group:test.surfconext.nl:nl:surfnet:diensten:surfwired-admin': {'permissions': [{'methods': ['GET', 'PUT'], 'endpoints': ['/devices/**/interfaces', '/repository']}, {'methods': ['POST'], 'endpoints': ['/auth/*', '/devices']}]}, 'default': {'permissions': [{'methods': ['GET'], 'endpoints': ['/devices**']}]}}}
         user_info = {}
         permissions_of_user = get_permissions_user(permissions_rules, user_info)
-        expected_result = {'any': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs']}}
+        expected_result = [{'methods': ['GET'], 'endpoints': ['/devices**']}]
         self.assertEqual(permissions_of_user, expected_result)
     
     def test_user_no_element_zero(self):
-        permissions_rules = {'config': {'role_in_jwt_element': 'edumember_is_member_of', 'roles_separated_by': ':'}, 'roles': {'read': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs', '*']}, 'write': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['*']}, 'allowed_api_calls': ['/devices', '/jobs', '/repository/y.*']}, 'any': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs']}}
+        permissions_rules = {'config': {'group_claim_key': 'edumember_is_member_of'}, 'roles': {'urn:collab:group:test.surfconext.nl:nl:surfnet:diensten:surfwired-admin': {'permissions': [{'methods': ['GET', 'PUT'], 'endpoints': ['/devices/**/interfaces', '/repository']}, {'methods': ['POST'], 'endpoints': ['/auth/*', '/devices']}]}, 'default': {'permissions': [{'methods': ['GET'], 'endpoints': ['/devices**']}]}}}
         user_info = {}
         permissions_of_user = get_permissions_user(permissions_rules, user_info)
-        expected_result = {}
-        self.assertEqual(permissions_of_user, expected_result)
-
-    def test_rules_no_element_default(self):
-        permissions_rules = {'config': {'roles_separated_by': ':', 'default_permissions': 'any'}, 'roles': {'read': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs', '*']}, 'write': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['*']}, 'admin': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['/devices', '/jobs', '/repository/y.*']}, 'any': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs']}}}
-        user_info = {"edumember_is_member_of":"test"}
-        permissions_of_user = get_permissions_user(permissions_rules, user_info)
-        expected_result = {'any': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs']}}
-        self.assertEqual(permissions_of_user, expected_result)
-    
-    def test_rules_no_element_zero(self):
-        permissions_rules = {'config': {'roles_separated_by': ':'}, 'roles': {'read': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs', '*']}, 'write': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['*']}, 'allowed_api_calls': ['/devices', '/jobs', '/repository/y.*']}, 'any': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs']}}
-        user_info = {"edumember_is_member_of":"test"}
-        permissions_of_user = get_permissions_user(permissions_rules, user_info)
-        expected_result = {}
+        expected_result = []
         self.assertEqual(permissions_of_user, expected_result)
 
     def test_rules_empty(self):
         permissions_rules = {}
         user_info = {"edumember_is_member_of":"test"}
         permissions_of_user = get_permissions_user(permissions_rules, user_info)
-        expected_result = {}
+        expected_result = []
         self.assertEqual(permissions_of_user, expected_result)
    
     def test_role_permissions_list(self):
-        permissions_rules = {'config': {'role_in_jwt_element': 'edumember_is_member_of', 'roles_separated_by': ','}, 'roles': {'read': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs', '*']}, 'write': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['*']}, 'admin': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['/devices', '/jobs', '/repository/y.*']}, 'any': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs']}}}
-        user_info = {"edumember_is_member_of":["read,write", "admin"]}
+        permissions_rules = {'config': {'group_claim_key': 'edumember_is_member_of', 'default_permissions': 'default'}, 'roles': {'urn:collab:group:test.surfconext.nl:nl:surfnet:diensten:surfwired-admin': {'permissions': [{'methods': ['GET', 'PUT'], 'endpoints': ['/devices/**/interfaces', '/repository']}, {'methods': ['POST'], 'endpoints': ['/auth/*', '/devices']}]}, 'default': {'permissions': [{'methods': ['GET'], 'endpoints': ['/devices**']}]}}}
+        user_info = {"edumember_is_member_of":["test", "urn:collab:group:test.surfconext.nl:nl:surfnet:diensten:surfwired-admin"]}
         permissions_of_user = get_permissions_user(permissions_rules, user_info)
-        expected_result = {'write': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['*']}, 'read': {'allowed_api_methods': ['GET'], 'allowed_api_calls': ['/devices', '/jobs', '*']}, 'admin': {'allowed_api_methods': ['GET', 'PUT'], 'allowed_api_calls': ['/devices', '/jobs', '/repository/y.*']}}
+        expected_result =[{'methods': ['GET'], 'endpoints': ['/devices**']}, {'methods': ['GET', 'PUT'], 'endpoints': ['/devices/**/interfaces', '/repository']}, {'methods': ['POST'], 'endpoints': ['/auth/*', '/devices']}]
         self.assertEqual(permissions_of_user, expected_result)
 
 if __name__ == "__main__":

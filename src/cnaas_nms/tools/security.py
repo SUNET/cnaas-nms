af576daad1f88d2641ea97eeb938b37279764864
Johan Marcusson <johan.marcusson@sunet.se>
2024-02-14 10:07:36 +0100

    Add redis cache of oidc userinfo data so we don't need to query identity server for every request. This makes the API a lot more responsive

--- src/cnaas_nms/tools/security.py
+++ src/cnaas_nms/tools/security.py
@@ -1,5 +1,6 @@
 import json
-from typing import Any, Mapping
+import time
+from typing import Any, Mapping, Optional
 
 import requests
 from authlib.integrations.flask_oauth2 import ResourceProtector, current_token
@@ -9,13 +10,29 @@ from flask_jwt_extended import get_jwt_identity as get_jwt_identity_orig
 from flask_jwt_extended import jwt_required as jwt_orig
 from jose import exceptions, jwt
 from jwt.exceptions import ExpiredSignatureError, InvalidAudienceError, InvalidKeyError, InvalidTokenError
+from redis.exceptions import RedisError
 
 from cnaas_nms.app_settings import api_settings, auth_settings
+from cnaas_nms.db.session import redis_session
+from cnaas_nms.scheduler.scheduler import SingletonType
 from cnaas_nms.tools.log import get_logger
 from cnaas_nms.tools.rbac.rbac import check_if_api_call_is_permitted, get_permissions_user
 from cnaas_nms.tools.rbac.token import Token
 
 logger = get_logger()
+
+
+REDIS_OAUTH_USERINFO_KEY = "oauth_userinfo"
+
+
+class JWKSStore(object, metaclass=SingletonType):
+    keys: Mapping[str, Any]
+
+    def __init__(self, keys: Optional[Mapping[str, Any]] = None):
+        if keys:
+            self.keys = keys
+        else:
+            self.keys = {}
 
 
 def jwt_required(fn):
@@ -46,9 +63,18 @@ def get_oauth_userinfo(token: Token) -> Any:
         resp.json(): Object of the user info
 
     """
-    # For now unnecersary, useful when we only use one log in method
+    # For now unnecessary, useful when we only use one log in method
     if not auth_settings.OIDC_ENABLED:
         return None
+    # Check if the userinfo is in the cache to avoid multiple calls to the OIDC server
+    try:
+        with redis_session() as redis:
+            cached_userinfo = redis.hget(REDIS_OAUTH_USERINFO_KEY, token.decoded_token["sub"])
+            if cached_userinfo:
+                return json.loads(cached_userinfo)
+    except RedisError as e:
+        logger.debug("Redis cache error: {}".format(str(e)))
+
     # Request the userinfo
     try:
         s = requests.Session()
@@ -64,6 +90,15 @@ def get_oauth_userinfo(token: Token) -> Any:
     try:
         resp = s.post(user_info_endpoint, data=data, headers=headers)
         resp.raise_for_status()
+        resp.json()
+        with redis_session() as redis:
+            if "exp" in token.decoded_token:
+                redis.hsetnx(REDIS_OAUTH_USERINFO_KEY, token.decoded_token["sub"], resp.text)
+                # expire hash at access_token expiry time or 1 hour from now
+                # (whichever is sooner)
+                # Entire hash is expired, since redis does not support expiry on individual keys
+                expire_at = min(int(token.decoded_token["exp"]), int(time.time()) + 3600)
+                redis.expireat(REDIS_OAUTH_USERINFO_KEY, when=expire_at, lt=True)
     except requests.exceptions.HTTPError as e:
         try:
             body = json.loads(e.response.content)
@@ -72,12 +107,14 @@ def get_oauth_userinfo(token: Token) -> Any:
         except (json.decoder.JSONDecodeError, KeyError):
             logger.debug("OIDC userinfo endpoint request not successful: {}".format(str(e.response.content)))
             raise InvalidTokenError(e.response.content)
+    except requests.exceptions.JSONDecodeError as e:
+        raise InvalidTokenError("Invalid JSON in userinfo response: {}".format(str(e)))
+    except RedisError as e:
+        logger.debug("Redis cache error: {}".format(str(e)))
     return resp.json()
 
 
 class MyBearerTokenValidator(BearerTokenValidator):
-    keys: Mapping[str, Any] = {}
-
     def get_keys(self):
         """Get the keys for the OIDC decoding"""
         try:
@@ -85,7 +122,8 @@ class MyBearerTokenValidator(BearerTokenValidator):
             metadata = s.get(auth_settings.OIDC_CONF_WELL_KNOWN_URL)
             keys_endpoint = metadata.json()["jwks_uri"]
             response = s.get(url=keys_endpoint)
-            self.keys = response.json()["keys"]
+            jwks_store = JWKSStore()
+            jwks_store.keys = response.json()["keys"]
         except KeyError as e:
             raise InvalidKeyError(e)
         except requests.exceptions.HTTPError as e:
@@ -93,15 +131,16 @@ class MyBearerTokenValidator(BearerTokenValidator):
 
     def get_key(self, kid):
         """Get the key based on the kid"""
-        key = [k for k in self.keys if k["kid"] == kid]
+        jwks_store = JWKSStore()
+        key = [k for k in jwks_store.keys if k["kid"] == kid]
         if len(key) == 0:
             logger.debug("Key not found. Get the keys.")
             self.get_keys()
-            if len(self.keys) == 0:
+            if len(jwks_store.keys) == 0:
                 logger.error("Keys not downloaded")
                 raise InvalidKeyError()
             try:
-                key = [k for k in self.keys if k["kid"] == kid]
+                key = [k for k in jwks_store.keys if k["kid"] == kid]
             except KeyError as e:
                 logger.error("Keys in different format?")
                 raise InvalidKeyError(e)

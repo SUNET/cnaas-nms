a81b321ba374f20bc31dd3101e74749977da609b
Johannes Jeppsson <johannes@sunet.se>
2024-06-10 10:56:56 +0200

    use docker v2

--- test/integrationtests.sh
+++ test/integrationtests.sh
@@ -22,7 +22,7 @@ export PYTEST_TEMPLATES_CLONED=1
 export JWT_AUTH_TOKEN="eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpYXQiOjE1NzEwNTk2MTgsIm5iZiI6MTU3MTA1OTYxOCwianRpIjoiNTQ2MDk2YTUtZTNmOS00NzFlLWE2NTctZWFlYTZkNzA4NmVhIiwic3ViIjoiYWRtaW4iLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.Sfffg9oZg_Kmoq7Oe8IoTcbuagpP6nuUXOQzqJpgDfqDq_GM_4zGzt7XxByD4G0q8g4gZGHQnV14TpDer2hJXw"
 export JWT_SECRET_KEY="integrationtestkey"
 
-docker-compose down
+docker compose down
 
 if docker volume ls | egrep -q "cnaas-postgres-data$"
 then
@@ -59,20 +59,20 @@ docker volume create cnaas-cacert
 
 set -e
 
-docker-compose up -d
+docker compose up -d
 
 docker cp ./jwt-cert/public.pem docker_cnaas_api_1:/opt/cnaas/jwtcert/public.pem
-docker-compose exec -u root -T cnaas_api /bin/chown -R www-data:www-data /opt/cnaas/jwtcert/
-docker-compose exec -u root -T cnaas_api /opt/cnaas/createca.sh
+docker compose exec -u root -T cnaas_api /bin/chown -R www-data:www-data /opt/cnaas/jwtcert/
+docker compose exec -u root -T cnaas_api /opt/cnaas/createca.sh
 
 curl --connect-timeout 2 --max-time 2 --retry 10 --retry-delay 0 --retry-max-time 60 \
     -ks "https://localhost/api/v1.0/system/version"
 
 # optional copy and restart
-docker-compose exec -u root -T cnaas_api /bin/mv /opt/cnaas/venv/cnaas-nms/src/ /opt/cnaas/venv/cnaas-nms/src-bundled
+docker compose exec -u root -T cnaas_api /bin/mv /opt/cnaas/venv/cnaas-nms/src/ /opt/cnaas/venv/cnaas-nms/src-bundled
 docker cp ../src docker_cnaas_api_1:/opt/cnaas/venv/cnaas-nms/src
-docker-compose exec -u root -T cnaas_api /bin/chown -R www-data:www-data /opt/cnaas/venv/cnaas-nms/src/
-docker-compose exec -u root -T cnaas_api /usr/bin/killall uwsgi
+docker compose exec -u root -T cnaas_api /bin/chown -R www-data:www-data /opt/cnaas/venv/cnaas-nms/src/
+docker compose exec -u root -T cnaas_api /usr/bin/killall uwsgi
 #
 
 if [ ! -z "$PRE_TEST_SCRIPT" ]
@@ -111,25 +111,25 @@ echo "Gathering coverage reports from integration tests:"
 MULE_PID="`docker logs docker_cnaas_api_1 | awk '/spawned uWSGI mule/{print $6}' | egrep -o "[0-9]+" | tail -n1`"
 echo "Found mule at pid $MULE_PID"
 # Allow for code coverage files to be saved
-docker-compose exec -u root -T cnaas_api chown -R www-data:www-data /opt/cnaas/venv/cnaas-nms/src/
+docker compose exec -u root -T cnaas_api chown -R www-data:www-data /opt/cnaas/venv/cnaas-nms/src/
 curl -ks -H "Authorization: Bearer $JWT_AUTH_TOKEN" "https://localhost/api/v1.0/system/shutdown" -d "{}" -X POST -H "Content-Type: application/json"
 sleep 3
 
 echo "Starting unit tests..."
-docker-compose exec -u www-data -T cnaas_api /opt/cnaas/pytest.sh
+docker compose exec -u www-data -T cnaas_api /opt/cnaas/pytest.sh
 echo "Try to generate coverage report:"
 if [ -z "$AUTOTEST" ]
 then
 	read -p "Do you want to upload coverage report to codecov.io? [y/N]" ans
 	case $ans in
-		[Yy]* ) docker-compose exec -u www-data -T cnaas_api /opt/cnaas/coverage.sh;;
+		[Yy]* ) docker compose exec -u www-data -T cnaas_api /opt/cnaas/coverage.sh;;
 		* ) echo "Not uploading coverage report";;
 	esac
 else
-	docker-compose exec -u www-data -T cnaas_api /opt/cnaas/coverage.sh
+	docker compose exec -u www-data -T cnaas_api /opt/cnaas/coverage.sh
 fi
 
 docker logs docker_cnaas_dhcpd_1
 docker logs docker_cnaas_api_1
 
-docker-compose down
+docker compose down

d1489a7ccddf86ccbdefd0c922ed365e62697c68
Jakob Mellberg <jakob.mellberg@gmail.com>
2025-12-08 19:28:49 +0000

    Import TypedDict from typing_extension for python < 3.12 to support aerleon

--- src/cnaas_nms/__init__.py
+++ src/cnaas_nms/__init__.py
@@ -1,3 +1,22 @@
+import sys
+
+"""
+Patches TypedDict so it is loaded from typing_extensions for python version < 3.12, eq 3.11
+Aerleon imports typing.TypedDict which is not supported with Pydantic.
+
+Error:
+pydantic.errors.PydanticUserError: Please use `typing_extensions.TypedDict` instead of `typing.TypedDict` on Python < 3.12.
+
+This can be removed when updating a version >= 3.12
+"""
+
+if sys.version_info < (3, 12):
+    import typing  # noqa: I001
+    from typing_extensions import TypedDict
+
+    typing.TypedDict = TypedDict  # override
+
+
 __import__("pkg_resources").declare_namespace(__name__)
 
 

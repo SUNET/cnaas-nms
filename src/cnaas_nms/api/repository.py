124b15cf05365e371ef50747686bbd9b3b12c198
Jakob Mellberg <jakob.mellberg@gmail.com>
2026-06-18 12:38:54 +0000

    Add a feature to check commits_ahead and commits_behind and update repository endpoint to hand out this information

--- src/cnaas_nms/api/repository.py
+++ src/cnaas_nms/api/repository.py
@@ -2,7 +2,7 @@ from flask import request
 from flask_restx import Namespace, Resource, fields
 
 from cnaas_nms.api.generic import empty_result
-from cnaas_nms.db.git import RepoType, get_repo_status, refresh_repo
+from cnaas_nms.db.git import RepoType, commits_out_of_sync, get_repo_status, refresh_repo
 from cnaas_nms.db.joblock import JoblockError
 from cnaas_nms.db.settings import SettingsSyntaxError, VerifyPathException
 from cnaas_nms.tools.security import get_identity, login_required
@@ -17,16 +17,50 @@ repository_model = api.model(
     },
 )
 
+repository_return_model = api.model(
+    "RepositoryReturn",
+    {
+        "status": fields.String(enum=["success", "error"], description="Status of the response"),
+        "data": fields.String(description="Commit information"),
+        "commits_ahead": fields.Integer(
+            description="How many commits ahead of origin",
+            required=False,
+            default=None,
+        ),
+        "commits_behind": fields.Integer(
+            description="How many commits behind of origin",
+            required=False,
+            default=None,
+        ),
+    },
+)
+
 
 class RepositoryApi(Resource):
+    @api.doc(
+        params={
+            "repo": {
+                "description": "Repository type",
+                "type": "string",
+                "enum": [r.name.lower() for r in RepoType],
+            }
+        }
+    )
+    @api.marshal_with(repository_return_model)
     @login_required
-    def get(self, repo):
+    def get(self, repo: str):
         """Get repository information"""
         try:
             repo_type = RepoType[str(repo).upper()]
         except Exception:  # noqa: S110
             return empty_result("error", "Invalid repository type"), 400
-        return empty_result("success", get_repo_status(repo_type))
+        result = empty_result("success", get_repo_status(repo_type))
+
+        ahead, behind = commits_out_of_sync(repo_type)
+        result["commits_ahead"] = ahead
+        result["commits_behind"] = behind
+
+        return result
 
     @login_required
     @api.expect(repository_model)
